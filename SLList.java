f2357fe9e83a30f630abcb7a8f6a88a00fcc0922
xixihaha1995 <43649779+xixihaha1995@users.noreply.github.com>
2020-02-16 18:23:12 -0500

    addint IntNode class

--- SLList.java
+++ SLList.java
@@ -1,22 +1,35 @@
+class IntNode{
+   public int item;
+   public IntNode next;
+   
+   public IntNode(int i, IntNode n){
+         item = i;
+         next = n;
+      }
+   }
+
 public class SLList {
    
    public IntNode first;
    
-   
-   
    public SLList(int x){
-      first = new IntNode(int x, null);
+      first = new IntNode(x, null);
    }
    
    public void addFirst(int x){
-      first = new IntNode(int x, first);
+      first = new IntNode(x, first);
    }
    
    public int getFirst(){
       return first.item;
    }
+ 
    
    public static void main(String[] args) {
       SLList L = new SLList(10);
+      L.addFirst(1);
+      L.addFirst(5);
+      System.out.println(L.getFirst());
    }
 }
+
