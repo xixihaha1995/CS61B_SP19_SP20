22eddcac6897641ef4fd0b4a6b806bf4e0512acd
xixihaha1995 <43649779+xixihaha1995@users.noreply.github.com>
2020-03-24 17:33:29 -0400

    Create oddNumber.java

---
+++ practice/oddNumber.java
@@ -0,0 +1,15 @@
+    public static List<Integer> oddNumbers(int l, int r) {
+    // Write your code here
+    List<Integer> list = new ArrayList<Integer>();
+    if(l%2==0) {
+        list.add(l+1);
+    }
+    else {
+
+        list.add(l);
+    }
+    for(int i=1;i<=list.size();i++) {
+        list.add(list.get(i-1)+2);
+    }
+    return list;
+    }
