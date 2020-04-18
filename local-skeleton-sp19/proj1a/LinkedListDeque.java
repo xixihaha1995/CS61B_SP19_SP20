597bd3b4c443ba5887a34b859a5069337dbdd707
wulicheneason <wulicheneason@local.com>
2020-04-18 17:39:23 -0400

    fix indentation

--- local-skeleton-sp19/proj1a/LinkedListDeque.java
+++ local-skeleton-sp19/proj1a/LinkedListDeque.java
@@ -1,23 +1,24 @@
 public class LinkedListDeque<genericType> {
-    public class IntNode{
+    private class IntNode {
         public IntNode prev;
         public genericType item;
         public IntNode next;
 
-        public IntNode<IntNode p, genericType i, IntNode n>{
-        	prev = p;
-        	item = i;
-        	next = n;
-        }
-
-        private IntNode sentinel;
-        private int size;
-
-        public LinkedListDeque(){
-        	sentinel = new IntNode(null, 6, null);
-        	sentinel.prev = sentinel;
-        	sentinel.next = sentinel;
-        	size = 0;
+        public IntNode(IntNode p, genericType i, IntNode n) {
+            prev = p;
+            item = i;
+            next = n;
         }
     }
+
+    private IntNode sentinel;
+    private int size;
+
+    public LinkedListDeque () {
+        sentinel = new IntNode(null, 6, null);
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+        size = 0;
+    }
 }
+
