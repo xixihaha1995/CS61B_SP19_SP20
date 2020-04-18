c30ada8997ed7fe50a376b3bbd9005273b017a82
wulicheneason <wulicheneason@local.com>
2020-04-18 17:54:35 -0400

    generic bug question mark

--- local-skeleton-sp19/proj1a/LinkedListDeque.java
+++ local-skeleton-sp19/proj1a/LinkedListDeque.java
@@ -1,17 +1,17 @@
 public class LinkedListDeque<genericType> {
-    private class IntNode{
+    private class IntNode<geTy>{
         public IntNode prev;
         public genericType item;
         public IntNode next;
 
-        public IntNode(IntNode p, genericType i, IntNode n){
+        public IntNode<>(IntNode p, genericType i, IntNode n){
             prev = p;
             item = i;
             next = n;
         }
     }
 
-    private IntNode<IntNode, genericType, IntNode> sentinel;
+    private IntNode<geTy> sentinel;
     private int size;
 
     public LinkedListDeque () {
