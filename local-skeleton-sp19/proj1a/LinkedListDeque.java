8d542049f8ea0855d48151fc9e3056f72fec83eb
wulicheneason <wulicheneason@local.com>
2020-04-18 17:34:05 -0400

    how to construct generic IntNode

--- local-skeleton-sp19/proj1a/LinkedListDeque.java
+++ local-skeleton-sp19/proj1a/LinkedListDeque.java
@@ -1,2 +1,23 @@
-public class LinkedListDeque {
+public class LinkedListDeque<genericType> {
+    public class IntNode{
+        public IntNode prev;
+        public genericType item;
+        public IntNode next;
+
+        public IntNode<IntNode p, genericType i, IntNode n>{
+        	prev = p;
+        	item = i;
+        	next = n;
+        }
+
+        private IntNode sentinel;
+        private int size;
+
+        public LinkedListDeque(){
+        	sentinel = new IntNode(null, 6, null);
+        	sentinel.prev = sentinel;
+        	sentinel.next = sentinel;
+        	size = 0;
+        }
+    }
 }
