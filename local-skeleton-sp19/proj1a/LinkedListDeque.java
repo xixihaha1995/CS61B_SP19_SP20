18be3b6b894442c5d2fab81dd67fd43c78c7a8bb
wulicheneason <wulicheneason@local.com>
2020-04-19 08:47:34 -0400

    sentinel within LinkedListDeque can be accessed with this.sentinel, although sentinel is a private variable.

--- local-skeleton-sp19/proj1a/LinkedListDeque.java
+++ local-skeleton-sp19/proj1a/LinkedListDeque.java
@@ -21,5 +21,65 @@ public class LinkedListDeque<genericType> {
         sentinel.next = sentinel;
         size = 0;
     }
+    public void addFirst(genericType i){
+        sentinel.next.prev = new genericNode(sentinel, i, sentinel.next);
+        sentinel.next = sentinel.next.prev;
+        size += 1;
+    }
+    public void addLast(genericType i){
+        sentinel.prev.next = new genericNode(sentinel.prev, i, sentinel);
+        sentinel.prev = sentinel.prev.next;
+        size += 1;
+    }
+    public boolean isEmpty(){
+        if (size == 0){
+            return true;
+        }
+    }
+    public int size(){
+        return size;
+    }
+    public void printDeque(){
+        genericNode p = sentinel.next;
+        while(p != sentinel){
+            System.out.print(p.item);
+            p = p.next;
+        }
+    }
+    public genericType removeFirst(){
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        size -= 1;
+        if (size>0){
+            return sentinel.next.item;
+        }else{
+            return null;
+        }
+    }
+    public genericType removeLast(){
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        size -=1;
+        if (size>0){
+            return sentinel.prev.item;
+        }else{
+            return null;
+        }
+    }
+    public genericType get(int index){
+        genericNode p = sentinel.next;
+        for(int i = 0; i< index; i++){
+            p = p.next;
+        }
+        return p.item;
+    }
+    public LinkedListDeque(LinkedListDeque other){
+        sentinel = new genericNode(null, null, null);
+        size = other.size();
+        addLast(other.get(i));
+        }
+
+    }
+
 }
 
