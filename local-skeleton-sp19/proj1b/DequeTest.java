2622e499a238e1adabb14ef1a778953f28870f0d
wulicheneason <wulicheneason@local.com>
2020-04-21 18:44:04 -0400

    assertEquals name synax

--- local-skeleton-sp19/proj1b/DequeTest.java
+++ local-skeleton-sp19/proj1b/DequeTest.java
@@ -4,10 +4,12 @@ import org.junit.Test;
 public class DequeTest {
     @Test
     public void testAddFirst(){
-        String input = 'cat';
+        String input = "cat";
         Deque<String> deque = new ArrayDeque<String>();
         deque.addFirst(input);
-        assertArrayEquals(input,deque.get(0));
+        String out = deque.get(0);
+        assertEquals(input,out);
     }
+
 
 }
