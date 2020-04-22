e9984fd2a588b902d0341258029cc7ad5eb6016f
wulicheneason <wulicheneason@local.com>
2020-04-21 20:07:55 -0400

    static Palindrome or non-static

--- local-skeleton-sp19/proj1b/Palindrome.java
+++ local-skeleton-sp19/proj1b/Palindrome.java
@@ -10,4 +10,32 @@ public class Palindrome {
         return deque;
     }
 
+    public boolean isPalindrome(String word){
+        if(wordToDeque(word).size() <2){
+            return true;
+        }else{
+            Deque<Character> rev = reverse(word);
+            return Palindrome.isEqual(wordToDeque(word),rev);
+        }
+    }
+    public boolean isEqual(Deque<Character> a, Deque<Character> b){
+        // TODO how to compare a and b
+        if (a == b){
+            return true;
+        }else {
+            return false;
+        }
+    }
+    public Deque<Character> reverse(String word){
+        Deque<Character> deque = new ArrayDeque<>();
+
+//        deque.addFirst(word.charAt(0));
+
+        for(int i = 0; i<word.length();i++) {
+            deque.addFirst(word.charAt(i));
+        }
+        return deque;
+    }
+
+
 }
