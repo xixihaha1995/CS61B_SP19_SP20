025867fdeb207a1282719a0b715c4b68bae186c4
wulicheneason <wulicheneason@local.com>
2020-04-23 15:30:32 -0400

    waiting ring buffer Data Structure

--- local-skeleton-sp19/hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
+++ local-skeleton-sp19/hw1/es/datastructur/synthesizer/ArrayRingBuffer.java
@@ -5,7 +5,7 @@ import java.util.Iterator;
 //TODO: Make sure to add the override tag for all overridden methods
 //TODO: Make sure to make this class implement BoundedQueue<T>
 
-public class ArrayRingBuffer<T>  {
+public class ArrayRingBuffer<T> implements BoundedQueue<T> {
     /* Index for the next dequeue or peek. */
     private int first;
     /* Index for the next enqueue. */
@@ -14,23 +14,38 @@ public class ArrayRingBuffer<T>  {
     private int fillCount;
     /* Array for storing the buffer data. */
     private T[] rb;
+    private int bufferCapacity;
+
+    public T[] items;
 
     /**
      * Create a new ArrayRingBuffer with the given capacity.
      */
-    public ArrayRingBuffer(int capacity) {
+    public ArrayRingBuffer(int x) {
         // TODO: Create new array with capacity elements.
         //       first, last, and fillCount should all be set to 0.
+        items = (T []) new Object[x];
+        first = 0;
+        last  = 0;
+        fillCount = 0;
     }
 
     /**
      * Adds x to the end of the ring buffer. If there is no room, then
      * throw new RuntimeException("Ring buffer overflow").
      */
+    public int fillCount() {
+        return fillCount;
+    }
+    public int capacity() {
+        return bufferCapacity;
+    }
     public void enqueue(T x) {
         // TODO: Enqueue the item. Don't forget to increase fillCount and update
         //       last.
-        return;
+        items[last] = x;
+        fillCount += 1;
+        last += 1;
     }
 
     /**
@@ -40,7 +55,22 @@ public class ArrayRingBuffer<T>  {
     public T dequeue() {
         // TODO: Dequeue the first item. Don't forget to decrease fillCount and
         //       update first.
-        return null;
+        if ( isEmpty() ) {
+            throw new RuntimeException("Ring buffer underflow");
+        } else {
+            int p = first;
+            T sto = items[first];
+            for (int i = 0; i < fillCount - 1; i++) {
+                items[p] = items[p++];
+            }
+            items[p] = null;
+            last -= 1;
+            fillCount -= 1;
+
+            return sto;
+
+        }
+
     }
 
     /**
@@ -50,7 +80,7 @@ public class ArrayRingBuffer<T>  {
     public T peek() {
         // TODO: Return the first item. None of your instance variables should
         //       change.
-        return null;
+        return items[first];
     }
 
     // TODO: When you get to part 4, implement the needed code to support
--- local-skeleton-sp19/hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
+++ local-skeleton-sp19/hw1/es/datastructur/synthesizer/TestArrayRingBuffer.java
@@ -9,6 +9,17 @@ import static org.junit.Assert.*;
 public class TestArrayRingBuffer {
     @Test
     public void someTest() {
-        //ArrayRingBuffer arb = new ArrayRingBuffer(10);
+        ArrayRingBuffer<Double> x = new ArrayRingBuffer(10);
+
+        x.enqueue(33.1) ;// 33.1 null null  null
+        x.enqueue(44.8) ;// 33.1 44.8 null  null
+        x.enqueue(62.3) ;// 33.1 44.8 62.3  null
+        x.enqueue(-3.4) ;// 33.1 44.8 62.3 -3.4
+        Double front  = x.dequeue();     // 44.8 62.3 -3.4  null (returns 33.1)
+        assertEquals(java.util.Optional.of(33.1), front);
+        assertEquals(java.util.Optional.of(44.8), x.peek());
+        assertEquals(java.util.Optional.of(44.8), x.dequeue());
+        assertFalse(x.isEmpty());
+
     }
 }
