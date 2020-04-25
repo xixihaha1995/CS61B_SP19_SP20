d07a42d373890c442b17c95b96545c262d2e36b5
wulicheneason <wulicheneason@local.com>
2020-04-25 15:07:58 -0400

    Is it necessary to instantiate boolean[]?

--- local-skeleton-sp19/hw2/hw2/Percolation.java
+++ local-skeleton-sp19/hw2/hw2/Percolation.java
@@ -3,16 +3,16 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
-    public int[][] block;
+//    public int[] block;
     public int open;
-    public boolean[][] markedOpen;
-    public boolean[][] markedFull;
-    public int[][] id;
+    public boolean[] markedOpen;
+    public boolean[] markedFull;
+    public int[] id;
 
     private int numberOfGrid;
     public Percolation(int N)                // create N-by-N grid, with all sites initially blocked
     {
-        block = new int[N][N];
+        id = new int[ N * N ];
         numberOfGrid = N;
 //        for (boolean b : markedFull[0]) {
 //            markedFull[0][b] = true;
@@ -20,6 +20,8 @@ public class Percolation {
     }
     public void open(int row, int col)       // open the site (row, col) if it is not open already
     {
+        validate(row, col);
+
         markedOpen[row][col] = true;
     }
     public boolean isOpen(int row, int col)  // is the site (row, col) open?
@@ -45,5 +47,18 @@ public class Percolation {
     }
         return false;
     }
+
+    private int xyTo1D(int row, int col) {
+        return row * col - 1;
+    }
+    private void validate ( int row, int col) {
+        if ( row < 0 || row > numberOfGrid ) {
+            throw new RuntimeException("Non-Valid Row Index");
+        }
+        if ( col < 0 || col > numberOfGrid ) {
+            throw new RuntimeException("Non-Valid Col Index");
+        }
+    }
+
     public static void main(String[] args)   // use for unit testing (not required, but keep this here for the autograder)
 }
