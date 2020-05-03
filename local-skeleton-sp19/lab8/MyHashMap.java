2a78e199e656afe145a7ee0dd7646d8168e60a32
lichenAtDell <wulicheneason@gmail.com>
2020-05-03 19:27:52 -0400

    how to add first item according to its key into ArrayDeque, and set its value

--- local-skeleton-sp19/lab8/MyHashMap.java
+++ local-skeleton-sp19/lab8/MyHashMap.java
@@ -1,16 +1,43 @@
+import java.util.Deque;
 import java.util.Iterator;
 import java.util.Set;
 
 public class MyHashMap<K,V> implements Map61B<K,V> {
+    private int sizeNum;
+    public Set<K> setForKeys;
+    private int initialSize;
+    private double loadFactor;
+//    private int buckNum;
+    private Deque<K>[] bucket;
+
+    public MyHashMap(){
+        this.initialSize = 16;
+        this.loadFactor = 0.75;
+        bucket = (Deque<K> []) new Object[initialSize];
+    }
+    public MyHashMap(int initialSize){}
+    public MyHashMap(int initialSize, double loadFactor){}
+
 
     @Override
     public void clear() {
-
+        setForKeys = null;
+        sizeNum = 0;
     }
 
     @Override
     public boolean containsKey(K key) {
-        return false;
+        return setForKeys.contains(key);
+    }
+    @Override
+    public void put(K key, V value) {
+        int curHash = key.hashCode();
+        int buckNum = ( curHash & 0x7FFFFFFF) % initialSize;
+        double curLoadFactor = bucket[buckNum].size()*1.0/sizeNum;
+        if (curLoadFactor < loadFactor) {
+            bucket[buckNum].
+        }
+
     }
 
     @Override
@@ -20,32 +47,33 @@ public class MyHashMap<K,V> implements Map61B<K,V> {
 
     @Override
     public int size() {
-        return 0;
+        return sizeNum;
     }
 
-    @Override
-    public void put(K key, V value) {
 
-    }
 
     @Override
     public Set<K> keySet() {
-        return null;
-    }
 
-    @Override
-    public V remove(K key) {
-        return null;
-    }
-
-    @Override
-    public V remove(K key, V value) {
-        return null;
+        return setForKeys;
     }
 
     @Override
     public Iterator<K> iterator() {
         return null;
     }
+
+
+
+    @Override
+    public V remove(K key) {
+        throw new UnsupportedOperationException();
+    }
+
+    @Override
+    public V remove(K key, V value) {
+        throw new UnsupportedOperationException();
+    }
+
 }
 
