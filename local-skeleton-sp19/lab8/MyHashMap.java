57c1e917912396a423fdfde99c5fb35fec67ab07
lichenAtDell <wulicheneason@gmail.com>
2020-05-04 00:55:50 -0400

    waiting for resizing

--- local-skeleton-sp19/lab8/MyHashMap.java
+++ local-skeleton-sp19/lab8/MyHashMap.java
@@ -6,13 +6,12 @@ public class MyHashMap<K,V> implements Map61B<K,V> {
     private int initialSize;
     private double loadFactor;
 //    private int buckNum;
-    private ArrayList<K> bucket;
+    private ArrayList<Entry<K,V>> bucket;
 
     public MyHashMap(){
         this.initialSize = 16;
         this.loadFactor = 0.75;
-        bucket.set()
-        bucket = (Deque<K> []) new Object[initialSize];
+        bucket = new ArrayList<Entry<K,V>>(initialSize);
     }
     public MyHashMap(int initialSize){}
     public MyHashMap(int initialSize, double loadFactor){}
@@ -30,31 +29,61 @@ public class MyHashMap<K,V> implements Map61B<K,V> {
     }
     @Override
     public void put(K key, V value) {
-        int curHash = key.hashCode();
-        int buckNum = ( curHash & 0x7FFFFFFF) % initialSize;
-        double curLoadFactor = bucket[buckNum].size()*1.0/sizeNum;
-        if (curLoadFactor < loadFactor) {
-            ArrayDeque<K> curArray = bucket[buckNum];
-            curArray.add(key);
-            curArray[key].element() = value;
-            setForKeys.add(key);
-        }
+/*        int curHash = key.hashCode();
+        int buckNum = ( curHash & 0x7FFFFFFF) % initialSize;*/
+
+        int h = hash (key);
+        Entry<K,V> e = find (key, bucket.get (h));
+        if (e == null) {
+            bucket.set (h, new Entry<K,V> (key, value, bucket.get (h)));
+            sizeNum += 1;
+            //TODO how to add load Factor , resizing
+ /*           if (sizeNum > bucket.size () * loadFactor) grow ();
+            return null;*/
+        } else
+             e.setValue (value);
+
+
+/*        double curLoadFactor = bucket.get(hash(key)).size()*1.0/sizeNum;
+        if ( !setForKeys.contains(key) ){
+            new bucket.set(hash(key), Entry<K,V>(key, value, null));
+        } else {
+
+        }*/
+
 
     }
-    private  class naiveHashMap{
+    private int hash(Object key) {
+        return ( key.hashCode() & 0x7FFFFFFF) % bucket.size();
+    }
+    private static class Entry<K,V>{
         K keyNaive;
         V valueNaive;
-        int sizeNaive;
-        public naiveHashMap(K key, V value){
-            keyNaive = key;
-            valueNaive = value;
+        Entry<K, V> next;
+        public Entry(K key, V value, Entry<K,V> next){
+            this.keyNaive = key;
+            this.valueNaive = value;
+            this.next = next;
+        }
+        public V setValue (V x) {
+            V old = valueNaive;
+            valueNaive = x;
+            return old;
         }
 
     }
+    private Entry<K,V> find (Object key, Entry<K,V> bin) {
+        for (Entry<K,V> e = bin; e != null; e = e.next)
+            if (key == null && e.keyNaive == null || key.equals (e.keyNaive))
+                return e;
+        return null;
+    }
+
 
     @Override
-    public V get(K key) {
-        return null;
+    public V get(Object key) {
+        Entry<K,V> e = find(key, bucket.get(key.hashCode()));
+        return e.valueNaive;
     }
 
     @Override
---
+++ local-skeleton-sp19/lab8/bucket/set.java
@@ -0,0 +1,6 @@
+package bucket;
+
+public class set {
+    public <V> set(int buckNum, boolean b, boolean b1, V value, Object o) {
+    }
+}
