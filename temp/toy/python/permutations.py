edc3af0aa00c54268d8c165017ccc50df7155b46
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 13:50:39 -0400

    sytax debug for python

--- temp/toy/python/permutations.py
+++ temp/toy/python/permutations.py
@@ -4,3 +4,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        self.res = []
+        def dfs(nums, temp):
+            if len(nums) == len(temp):
+                self.res.append(temp[:])
+                return
+            for i xrange(len(nums)):
+                if (i in temp):
+                    continue
+                temp.append(nums[i])
+                dfs(nums,temp)
+                temp.pop()
+        dfs(nums,[])
+        return self.res
