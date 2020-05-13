6515e251caf56dd6ac9b8c5940ebf404717828f4
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 15:32:25 -0400

    that's it how to skip second 1,2 but first 1,2,2

--- temp/toy/python/90. Subsets II.py
+++ temp/toy/python/90. Subsets II.py
@@ -5,7 +5,7 @@ class Solution(object):
     def __init__(self):
         self.res = []
 
-    def subsets(self, nums):
+    def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
         :rtype: List[List[int]]
@@ -15,6 +15,8 @@ class Solution(object):
             self.res.append(temp[:])
 
             for i in xrange(index, len(nums)):
+                if i > index and nums[i] == nums[i-1]:
+                    continue
                 temp.append(nums[i])
 
                 dfs(nums, temp, i+1)
@@ -25,4 +27,4 @@ class Solution(object):
 
 
 if __name__ == '__main__':
-    print(Solution().subsets([1, 2, 3]))
+    print(Solution().subsetsWithDup([1, 2, 2]))
