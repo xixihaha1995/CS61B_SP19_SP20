c02e630cdc727e03baabd361c10fee8e16a0bd9b
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 19:14:57 -0400

    syntax for 77 combinations

--- temp/toy/python/77. Combinations.py
+++ temp/toy/python/77. Combinations.py
@@ -3,23 +3,26 @@ from xlwt.compat import xrange
 
 class Solution(object):
 
+    def __init__(self):
+        self.res =[]
+        self.helper(range( 1, n+1), k, self.res, [])
+
     def combine(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: List[List[int]]
         """
-        res = []
-        self.helper  = (range( 1, n+1), k, path)
 
-        def helper(array, k, path):
+
+        def helper(array, k, res, path):
             if k > len(array):
                 return
             if k == 0:
-                return
-            else
+                self.res.append(path)
+            else:
                 for i in array:
-                    self.helper(array[i+1:], k - 1, path + array[i])
+                    self.helper(array[i+1:], k - 1, res, path + [array[i]])
 
 
 if __name__ == '__main__':
