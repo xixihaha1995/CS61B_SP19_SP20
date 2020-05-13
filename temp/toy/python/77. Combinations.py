5a9a21b0db6223b5fa1e371a6803049355bde731
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 19:24:13 -0400

    self global methods

--- temp/toy/python/77. Combinations.py
+++ temp/toy/python/77. Combinations.py
@@ -3,9 +3,6 @@ from xlwt.compat import xrange
 
 class Solution(object):
 
-    def __init__(self):
-        self.res =[]
-        self.helper(range( 1, n+1), k, self.res, [])
 
     def combine(self, n, k):
         """
@@ -13,17 +10,19 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
+        res = []
+        self.helper(range(1, n + 1), k, res, [])
+        return res
 
-
-        def helper(array, k, res, path):
-            if k > len(array):
-                return
-            if k == 0:
-                self.res.append(path)
-            else:
-                for i in array:
-                    self.helper(array[i+1:], k - 1, res, path + [array[i]])
+    def helper(array, k, res, path):
+        if k > len(array):
+            return
+        if k == 0:
+            res.append(path)
+        else:
+            for i in range(len(array)):
+                self.helper(array[i + 1:], k - 1, res, path + [array[i]])
 
 
 if __name__ == '__main__':
-    print(Solution().combine(4,2))
+    print(Solution().combine(4, 2))
