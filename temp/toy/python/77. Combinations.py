e7ba44e4b2be28e775beeb9aee4bf1bed580ed28
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 19:11:11 -0400

    helper from python

--- temp/toy/python/77. Combinations.py
+++ temp/toy/python/77. Combinations.py
@@ -2,32 +2,24 @@ from xlwt.compat import xrange
 
 
 class Solution(object):
-    def __init__(self):
-        self.res = []
+
     def combine(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: List[List[int]]
         """
-        def dfs(nums, temp, count):
-            if count == 1:
-                self.res.append(temp)
+        res = []
+        self.helper  = (range( 1, n+1), k, path)
+
+        def helper(array, k, path):
+            if k > len(array):
                 return
-
-            for i in nums:
-                if i in temp:
-                    continue
-                temp.append(i)
-                nums.remove(i)
-                dfs(nums, temp, count - 1)
-
-        nums = []
-        for i in xrange(1,n+1):
-            nums.append(i)
-        dfs(nums, [], k)
-        return self.res
-
+            if k == 0:
+                return
+            else
+                for i in array:
+                    self.helper(array[i+1:], k - 1, path + array[i])
 
 
 if __name__ == '__main__':
