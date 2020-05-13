c442ba4135ba4a8aaee8c84610cae0f16b0899b8
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 17:42:19 -0400

    how to auto get sorted nums only from n

--- temp/toy/python/77. Combinations.py
+++ temp/toy/python/77. Combinations.py
@@ -1,7 +1,24 @@
 class Solution(object):
+    def __init__(self):
+        self.res = []
     def combine(self, n, k):
         """
         :type n: int
         :type k: int
         :rtype: List[List[int]]
         """
+        def dfs(remainder, temp, count):
+            if count == 0:
+                self.res.append(temp)
+
+            for i in remainder:
+                if i in temp:
+                    continue
+                temp.append(i)
+                remainder.remove(i)
+                dfs(remainder, temp, count - 1)
+
+       dfs(n,[], k)
+
+if __name__ == '__main__':
+    print(Solution().combine(4,2))
