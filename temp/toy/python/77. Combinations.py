b6d790b3b7aecdc239ee344174a438af5c59bc83
lichenAtDell <wulicheneason@gmail.com>
2020-05-13 17:57:13 -0400

    looking for reference solutions

--- temp/toy/python/77. Combinations.py
+++ temp/toy/python/77. Combinations.py
@@ -1,3 +1,6 @@
+from xlwt.compat import xrange
+
+
 class Solution(object):
     def __init__(self):
         self.res = []
@@ -7,18 +10,25 @@ class Solution(object):
         :type k: int
         :rtype: List[List[int]]
         """
-        def dfs(remainder, temp, count):
-            if count == 0:
+        def dfs(nums, temp, count):
+            if count == 1:
                 self.res.append(temp)
+                return
 
-            for i in remainder:
+            for i in nums:
                 if i in temp:
                     continue
                 temp.append(i)
-                remainder.remove(i)
-                dfs(remainder, temp, count - 1)
+                nums.remove(i)
+                dfs(nums, temp, count - 1)
 
-       dfs(n,[], k)
+        nums = []
+        for i in xrange(1,n+1):
+            nums.append(i)
+        dfs(nums, [], k)
+        return self.res
+
+
 
 if __name__ == '__main__':
     print(Solution().combine(4,2))
