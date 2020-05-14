f6a1e411a2f9d16bf303de115587a09e43cfd63d
lichenAtDell <wulicheneason@gmail.com>
2020-05-14 19:46:48 -0400

    backtracking

--- temp/toy/python/131. Palindrome Partitioning.py
+++ temp/toy/python/131. Palindrome Partitioning.py
@@ -11,8 +11,8 @@ class Solution(object):
 
     def helper(self, s, res,  path):
         if not s:
-            res.append(path)
-            return
+            res.append(path[:])
+            # return
         for i in range(1, len(s) + 1):
             if self.isPalindrome(s[:i]):
                 path.append(s[:i])
