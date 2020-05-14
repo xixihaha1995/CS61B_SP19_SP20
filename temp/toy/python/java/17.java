edbe077cf2526025f18e828c5881db2d7460cb6b
lichenAtDell <wulicheneason@gmail.com>
2020-05-14 14:25:12 -0400

    debug for conditions

---
+++ temp/toy/python/java/17.java
@@ -0,0 +1,42 @@
+class Solution {
+	public List<String> letterCombinations(String digits) {
+		String[] d = new String[]
+				{
+						" ",
+						"",
+						"abc",
+						"def",
+						"ghi",
+						"jkl",
+						"mno",
+						"pqrs",
+						"tuv",
+						"wxyz"
+				};
+		List<String> ans = new ArrayList<>();
+		char[] curLettCombo = new char[digits.length()];
+		dfs(digits, d, ans, curLettCombo, 0);
+		return ans;
+
+
+	}
+	private void dfs(String digits, String[] d, List<String> ans, char[] curLettCombo, int curIndex)
+	{
+		if ( curIndex == digits.length()){
+			if (curIndex > 0) {
+				ans.add(new String(curLettCombo));
+			};
+			return;
+		}
+		String s = d[Character.getNumericValue(digits.charAt(curIndex))];
+		for (int i = curIndex; i < s.length(); ++i) {
+			curLettCombo[curIndex] = s.charAt(i);
+			dfs(digits, d, ans, curLettCombo, curIndex + 1);
+//    		curL
+//    		Char c = s.get(i);
+//    		curLettCombo.add(c);
+
+		}
+
+	}
+}
