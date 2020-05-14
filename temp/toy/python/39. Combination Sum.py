b01525a3e05a25cf4544006689f0e4e0dfa36c73
lichenAtDell <wulicheneason@gmail.com>
2020-05-14 10:25:52 -0400

    backtrack for combination sum

--- temp/toy/python/39. Combination Sum.py
+++ temp/toy/python/39. Combination Sum.py
@@ -6,15 +6,20 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         res = []
-        self.helper(candidates, temp, target, index)
+        self.helper(candidates, res, target, 0, [])
         return res
 
-    def helper(self, candidates, temp, target, index):
+    def helper(self, candidates, res, target, index, path):
         if (target < 0):
             return
         if (target == 0):
-            res.append(temp)
-        for i in range (index, len(candidates)):
-            temp.append(candidates[i])
-            self.helper(candidates,temp,target-candidates[i],i)
-            temp.pop()
+            res.append(path)
+        for i in range(index, len(candidates)):
+            path.append(candidates[i])
+            self.helper(candidates, res, target - candidates[i], i, path)
+            path.pop()
+            # temp.pop()
+
+
+if __name__ == '__main__':
+    print(Solution().combinationSum([2, 3, 6, 7], 7))
