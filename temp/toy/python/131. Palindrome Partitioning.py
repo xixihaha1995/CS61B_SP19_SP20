5dd09591747869c965b100b7894ceba9a5d9550a
lichenAtDell <wulicheneason@gmail.com>
2020-05-14 19:38:29 -0400

    conditions is the current substring is palidrome

--- temp/toy/python/131. Palindrome Partitioning.py
+++ temp/toy/python/131. Palindrome Partitioning.py
@@ -4,3 +4,14 @@ class Solution(object):
         :type s: str
         :rtype: List[List[str]]
         """
+        self.isPalindrome = lambda s : s == s[::-1]
+        res = []
+        self.helper(s, res, 0, [])
+        return res
+    def helper(self, s, res, index, path):
+        if not s:
+            res.append(path)
+        for i in range(1, len(s) + 1):
+            if isPalindrome(s[:i]):
+                self.helper(s[i:],res, i + 1, path + [s[:i]])
+
