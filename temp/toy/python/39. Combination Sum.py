ba53e16945afd8711f0fd3cbf061f564d3acd50c
lichenAtDell <wulicheneason@gmail.com>
2020-05-14 10:29:25 -0400

    path[:]

--- temp/toy/python/39. Combination Sum.py
+++ temp/toy/python/39. Combination Sum.py
@@ -9,14 +9,14 @@ class Solution(object):
         self.helper(candidates, res, target, 0, [])
         return res
 
-    def helper(self, candidates, res, target, index, path):
-        if (target < 0):
+    def helper(self, candidates, res, remainder, index, path):
+        if (remainder < 0):
             return
-        if (target == 0):
-            res.append(path)
+        if (remainder == 0):
+            res.append(path[:])
         for i in range(index, len(candidates)):
             path.append(candidates[i])
-            self.helper(candidates, res, target - candidates[i], i, path)
+            self.helper(candidates, res, remainder - candidates[i], i, path)
             path.pop()
             # temp.pop()
 
