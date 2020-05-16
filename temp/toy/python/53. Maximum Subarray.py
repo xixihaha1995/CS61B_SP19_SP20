25703a6cd5a69a96f2704b86f382371f6574b909
lichenAtDell <wulicheneason@gmail.com>
2020-05-16 17:49:59 -0400

    debug for max Subarray

--- temp/toy/python/53. Maximum Subarray.py
+++ temp/toy/python/53. Maximum Subarray.py
@@ -1,2 +1,7 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        res = 0
+        for num in nums:
+            num = max(res+num, num)
+            res = max(res, res + num)
+        return res
