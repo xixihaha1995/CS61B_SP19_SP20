4444bbd49364f93d70d87f05c280ad316b0a9ca2
lichenAtDell <wulicheneason@gmail.com>
2020-05-16 15:00:08 -0400

    why add cost(-1) = 0

--- temp/toy/python/746. Min Cost Climbing Stairs.py
+++ temp/toy/python/746. Min Cost Climbing Stairs.py
@@ -5,12 +5,14 @@ class Solution(object):
         :rtype: int
         """
         dp = [0] * (len(cost)+1)
+        cost.append(0)
+
         dp[0] = cost[0]
         dp[1] = cost[1]
-        for i in range(2,len(cost)+1):
+        for i in range(2,len(cost)):
             dp[i] = min(dp[i-1],dp[i-2])+ cost[i]
         return dp[-1]
 
 
 if __name__ == '__main__':
-    print(Solution().minCostClimbingStairs( [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
+    print(Solution().minCostClimbingStairs(  [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
