7dc2394b8effd94eaedc1abf3a89c67d77a7f61b
lichenAtDell <wulicheneason@gmail.com>
2020-05-16 14:53:04 -0400

    costl.append(0)

--- temp/toy/python/746. Min Cost Climbing Stairs.py
+++ temp/toy/python/746. Min Cost Climbing Stairs.py
@@ -4,13 +4,13 @@ class Solution(object):
         :type cost: List[int]
         :rtype: int
         """
-        dp = []
+        dp = [0] * (len(cost)+1)
         dp[0] = cost[0]
         dp[1] = cost[1]
-        for i in range(2,len(cost)):
-            dp[i] = min(dp[i-1],dp[i-2]) + cost[i]
-        return dp[len(cost)-1]
+        for i in range(2,len(cost)+1):
+            dp[i] = min(dp[i-1],dp[i-2])+ cost[i]
+        return dp[-1]
 
 
 if __name__ == '__main__':
-    print(Solution().minCostClimbingStairs( [10, 15, 20]))
+    print(Solution().minCostClimbingStairs( [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]))
