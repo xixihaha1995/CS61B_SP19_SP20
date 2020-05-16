868d457a12c450772da6e652d90f69d5633358e1
lichenAtDell <wulicheneason@gmail.com>
2020-05-16 17:19:04 -0400

    maximum subarray

--- temp/toy/python/1218. Longest Arithmetic Subsequence of Given Difference.py
+++ temp/toy/python/1218. Longest Arithmetic Subsequence of Given Difference.py
@@ -1,10 +1,14 @@
+from typing import List
+
+
 class Solution:
     def longestSubsequence(self, arr: List[int], difference: int) -> int:
         res = {}
-        for x in xrange(arr):
-            if x - difference in res:
-                res.append(x)
-            else:
-                break
-        return len(res)
+        for num in arr :
+            res[num] = res[num-difference] + 1 if num - difference in res else 1
+        return max(res.values())
+
+if __name__ == '__main__':
+    print(Solution().longestSubsequence([1,5,7,8,5,3,4,2,1], -2))
+
 
