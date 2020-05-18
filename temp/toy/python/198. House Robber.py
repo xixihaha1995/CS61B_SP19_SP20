d28a9a9e128fcfd8ab2c0fecde5bc4912fce1671
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 15:25:47 -0400

    debug for test cases

--- temp/toy/python/198. House Robber.py
+++ temp/toy/python/198. House Robber.py
@@ -7,10 +7,10 @@ class Solution:
             return 0
         dp = [0] * len(nums)
         for i in range(len(nums)):
-            dp[i] = max( dp[i-1] if i>0 else nums[i] , dp[i-2]+nums[i] if i > 1 else 0)
+            dp[i] = max( dp[i-1] if i>0 else nums[i] , dp[i-2]+nums[i] if i > 1 else nums[i])
         return dp[-1]
 
 if __name__ == '__main__':
     print(Solution().rob(
-        [1,2,3,1]
+        [2,1]
     ))
