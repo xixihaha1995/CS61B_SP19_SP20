e656209f901c703332bc800b7dde9ff0df781abe
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 17:31:54 -0400

    matrix[i][j] to int

--- temp/toy/python/221. Maximal Square.py
+++ temp/toy/python/221. Maximal Square.py
@@ -9,11 +9,16 @@ class Solution:
         n = len(matrix[0])
         dp = [[0]* n for _ in range(m)]
         for i in range(m):
-            dp[i][0] = matrix[i][0]
-        for i in range(n):
-            dp[0][j] = matrix[0][j]
+            dp[i][0] = int(matrix[i][0])
+        for j in range(n):
+            dp[0][j] = int(matrix[0][j])
         for i in range(1,m):
             for j in range(1,n):
-                if matrix[i][j] == 1:
+                if int(matrix[i][j]) == 1:
                     dp[i][j] = min(dp[i-1][j], dp[i][j-1],dp[i-1][j-1])+1
-        return map(max, dp)**2
+        return max(map(max, dp))**2
+
+if __name__ == '__main__':
+    print(Solution().maximalSquare(
+
+    ))
