a1c23d87c3eb87f50df2c941acdb3392baa33eb4
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 17:10:43 -0400

    boundary conditions

--- temp/toy/python/304. Range Sum Query 2D - Immutable.py
+++ temp/toy/python/304. Range Sum Query 2D - Immutable.py
@@ -11,13 +11,13 @@ class NumMatrix:
             m = len(matrix)
             n = len(matrix[0])
         self.sumM = [[0]*(n+1) for _ in range(m+1)]
-        for i in range(m + 1):
-            for j in range(n+1):
-                self.sumM[i+1][j+1] = self.sumM[i+1][j] + self.sumM[i][j+1] - self.sumM[i][j] + matrix[i+1][j+1]
+        for i in range(m):
+            for j in range(n):
+                self.sumM[i+1][j+1] = self.sumM[i+1][j] + self.sumM[i][j+1] - self.sumM[i][j] + matrix[i][j]
 
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        return self.sumM[row2 + 1][col2 + 1] - self.sumM[row2+1][col1+1] - self.sumM[row1+1][col2+1] + self.sumM[row1+1][col1+1]
+        return self.sumM[row2 + 1][col2 + 1] - self.sumM[row2+1][col1] - self.sumM[row1][col2+1] + self.sumM[row1][col1]
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
