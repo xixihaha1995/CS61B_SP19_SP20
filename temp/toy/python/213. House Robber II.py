cea91b1cbf4159a3542670a4cf8249069186094b
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 18:07:22 -0400

    negative slice for array in python

--- temp/toy/python/213. House Robber II.py
+++ temp/toy/python/213. House Robber II.py
@@ -9,7 +9,8 @@ class Solution:
             return nums[0]
         if len(nums) == 2:
             return max(nums[0], nums[1])
-        return max(self.robrange(nums[0:-2]), self.robrange(nums[1:-1]))
+        n = len (nums)
+        return max(self.robrange(nums[0:n-1]), self.robrange(nums[1:n]))
 
     def robrange(self, nums):
         if not nums:
@@ -23,6 +24,8 @@ class Solution:
         for i in range(n):
             dp[i] = max(dp[i - 1] if i > 0 else nums[i], dp[i - 2] + nums[i] if i > 1 else nums[i])
         return dp[-1]
+
+
 
 if __name__ == '__main__':
     print(Solution().rob(
