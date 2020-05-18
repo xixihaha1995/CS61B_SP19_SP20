b01ae912e94674cd016c9e39115f2afc50d7294e
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 18:02:13 -0400

    debugging

--- temp/toy/python/213. House Robber II.py
+++ temp/toy/python/213. House Robber II.py
@@ -8,22 +8,21 @@ class Solution:
         if len(nums) == 1:
             return nums[0]
         if len(nums) == 2:
-            return max(nums)
+            return max(nums[0], nums[1])
         return max(self.robrange(nums[0:-2]), self.robrange(nums[1:-1]))
 
-
     def robrange(self, nums):
-            if not nums:
-                return 0
-            if len(nums) == 1:
-                return nums[0]
-            if len(nums) == 2:
-                return max(nums)
-            n = len(nums)
-            dp = [0] * n
-            for i in range(1,n):
-                dp[i] = max(dp[i-1] if i>0 else nums[i], dp[i-2] if i>1 else nums[i])
-            return dp[-1]
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+        if len(nums) == 2:
+            return max(nums[0], nums[1])
+        n = len(nums)
+        dp = [0] * n
+        for i in range(n):
+            dp[i] = max(dp[i - 1] if i > 0 else nums[i], dp[i - 2] + nums[i] if i > 1 else nums[i])
+        return dp[-1]
 
 if __name__ == '__main__':
     print(Solution().rob(
