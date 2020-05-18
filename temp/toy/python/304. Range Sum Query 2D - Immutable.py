4123b004f1a8e3b780ab448dd8c33f997c05b5ac
lichenAtDell <wulicheneason@gmail.com>
2020-05-18 17:07:21 -0400

    rec area subregion

--- temp/toy/python/304. Range Sum Query 2D - Immutable.py
+++ temp/toy/python/304. Range Sum Query 2D - Immutable.py
@@ -13,10 +13,11 @@ class NumMatrix:
         self.sumM = [[0]*(n+1) for _ in range(m+1)]
         for i in range(m + 1):
             for j in range(n+1):
-                self.sumM[i][j] =
+                self.sumM[i+1][j+1] = self.sumM[i+1][j] + self.sumM[i][j+1] - self.sumM[i][j] + matrix[i+1][j+1]
 
 
     def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
+        return self.sumM[row2 + 1][col2 + 1] - self.sumM[row2+1][col1+1] - self.sumM[row1+1][col2+1] + self.sumM[row1+1][col1+1]
 
 # Your NumMatrix object will be instantiated and called as such:
 # obj = NumMatrix(matrix)
