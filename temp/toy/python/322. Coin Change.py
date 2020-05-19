5054b88acc449562a8b0699f0a1b609df92ca9c4
lichenAtDell <wulicheneason@gmail.com>
2020-05-19 10:08:22 -0400

    dynamic programming but stop when remainder is zero

--- temp/toy/python/322. Coin Change.py
+++ temp/toy/python/322. Coin Change.py
@@ -5,19 +5,20 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         self.res = 0
         self.dp(sorted(coins), amount)
-
         return self.res
 
     def dp(self, coins, remainder):
         if remainder < 0:
             self.res = -1
-            return
+            return self.res
         if remainder == 0:
-            return
-        for i in range(len(coins)-1,0,-1):
+            return self.res
+        for i in range(len(coins)-1,-1,-1):
+            # print(coins[i])
             if coins[i] <= remainder:
                 self.res += 1
                 self.dp(coins, remainder-coins[i])
+                break
 
 if __name__ == '__main__':
     print(Solution().coinChange(
