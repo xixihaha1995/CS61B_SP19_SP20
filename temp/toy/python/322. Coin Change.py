e5bb2ddf42cf1c466d3b74fdcd1867ed9703652e
lichenAtDell <wulicheneason@gmail.com>
2020-05-19 10:16:50 -0400

    my solution is wrong, please ref reference solution

--- temp/toy/python/322. Coin Change.py
+++ temp/toy/python/322. Coin Change.py
@@ -3,16 +3,20 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        if not coins:
+            return 0
         self.res = 0
         self.dp(sorted(coins), amount)
         return self.res
 
     def dp(self, coins, remainder):
-        if remainder < 0:
-            self.res = -1
-            return self.res
         if remainder == 0:
             return self.res
+
+        if remainder < 0 or remainder<coins[0]:
+            self.res = -1
+            return self.res
+
         for i in range(len(coins)-1,-1,-1):
             # print(coins[i])
             if coins[i] <= remainder:
@@ -22,5 +26,5 @@ class Solution:
 
 if __name__ == '__main__':
     print(Solution().coinChange(
-        [1, 2, 5], 11
+        [186, 419, 83, 408], 6249
     ))
