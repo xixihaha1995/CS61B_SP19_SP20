2071829ebdb346504a25737cee37eeeeb884d079
lichenAtDell <wulicheneason@gmail.com>
2020-05-19 10:31:04 -0400

    debug for syntax

--- temp/toy/python/322. Coin Change.py
+++ temp/toy/python/322. Coin Change.py
@@ -3,26 +3,11 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        if not coins:
-            return 0
-        self.res = 0
-        self.dp(sorted(coins), amount)
-        return self.res
-
-    def dp(self, coins, remainder):
-        if remainder == 0:
-            return self.res
-
-        if remainder < 0 or remainder<coins[0]:
-            self.res = -1
-            return self.res
-
-        for i in range(len(coins)-1,-1,-1):
-            # print(coins[i])
-            if coins[i] <= remainder:
-                self.res += 1
-                self.dp(coins, remainder-coins[i])
-                break
+        dp = [float 'inf'] * (amount+1)
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] = min(dp[i], dp[i-coin]+1)
+        return -1 if dp[-1] == float 'inf' else dp[-1]
 
 if __name__ == '__main__':
     print(Solution().coinChange(
