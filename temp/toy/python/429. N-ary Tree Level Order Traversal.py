8d347252241bb2485e32d929c990fa4fa71ee099
lichenAtDell <wulicheneason@gmail.com>
2020-05-19 13:05:04 -0400

    data structure for python

--- temp/toy/python/429. N-ary Tree Level Order Traversal.py
+++ temp/toy/python/429. N-ary Tree Level Order Traversal.py
@@ -1,10 +1,10 @@
-"""
+
 # Definition for a Node.
 class Node(object):
     def __init__(self, val=None, children=None):
         self.val = val
         self.children = children
-"""
+
 
 
 class Solution(object):
@@ -13,3 +13,21 @@ class Solution(object):
         :type root: Node
         :rtype: List[List[int]]
         """
+        if not root:
+            return []
+        # queue utility and
+        queue = [(root,0)]
+        res = [[]]
+        while queue:
+            node, level = queue.pop(0)
+            if level >= len(res):
+                res.append([])
+            res[level].append(node.val)
+            for child in node.children:
+                queue.append((child, level+1))
+        return res
+
+if __name__ == '__main__':
+    print(Solution().levelOrder(
+        [1,None,3,2,4,None,5,6]
+    ))
