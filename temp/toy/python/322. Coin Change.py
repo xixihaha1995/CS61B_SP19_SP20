ab3c3f23eb8d1277f6a0dec9ab724ee30ad103f3
lichenAtDell <wulicheneason@gmail.com>
2020-05-19 09:57:36 -0400

    debug for syntax

---
+++ temp/toy/python/322. Coin Change.py
@@ -0,0 +1,25 @@
+from typing import List
+
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        self.res = 0
+        self.dp(sorted(coins), amount)
+
+        return self.res
+
+    def dp(self, coins, remainder):
+        if remainder < 0:
+            self.res = -1
+            return
+        if remainder == 0:
+            return
+        for i in range(len(coins)-1,0,-1):
+            if coins[i] <= remainder:
+                self.res += 1
+                self.dp(coins, remainder-coins[i])
+
+if __name__ == '__main__':
+    print(Solution().coinChange(
+        [1, 2, 5], 11
+    ))
