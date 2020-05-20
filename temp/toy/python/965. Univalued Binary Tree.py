d79035b90a35f6c1baeb4fa06cbb4061ee9649f4
lichenAtDell <wulicheneason@gmail.com>
2020-05-20 07:34:52 -0400

    deque is useful when popleft needed

--- temp/toy/python/965. Univalued Binary Tree.py
+++ temp/toy/python/965. Univalued Binary Tree.py
@@ -6,4 +6,18 @@
 #         self.right = right
 class Solution:
     def isUnivalTree(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        q = collections.deque()
+        q.append(root)
+        val = root.val
+        while q:
+            node = q.popleft()
+            if not node:
+                continue
+            if node.val != val:
+                return False
+            q.append(node.left)
+            q.append(node.right)
+        return True
 
