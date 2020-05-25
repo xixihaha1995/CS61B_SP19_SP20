0ede6d5d0907053ff3f5faa59d8f60065dff1c2d
lichenAtDell <wulicheneason@gmail.com>
2020-05-25 08:42:55 -0400

    union the parent(left) with parent(right)

--- temp/toy/python/1202. Smallest String With Swaps.py
+++ temp/toy/python/1202. Smallest String With Swaps.py
@@ -1,13 +1,18 @@
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
         parent = list(range(len(s))
-        mem = collections.defaultdict(list)
+        def find(r):
+            while parent[r] != r:
+                parent[r] = find(parent[r])
+            return r
+
         for l, r in pairs:
             pl = find(l)
             pr = find(r)
             if pl != pr:
-                parent[l] = pr
+                parent[pl] = pr
 
+        mem = collections.defaultdict(list)
         for counter, value in enumerate(parent):
             mem[find(value)].append(s[counter])
         for sets in mem:
@@ -17,9 +22,6 @@ class Solution:
             res.append(mem[find(i)].pop())
         return res
 
-        def find(r):
-            while parent[r] != r:
-                parent[r] = find(parent[r])
-            return r
+
 
 
