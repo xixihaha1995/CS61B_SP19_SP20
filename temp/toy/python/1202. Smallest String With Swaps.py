0135b3c39dd5a4f4406bafd36317f1deff897f4b
lichenAtDell <wulicheneason@gmail.com>
2020-05-25 09:34:30 -0400

    what happened

--- temp/toy/python/1202. Smallest String With Swaps.py
+++ temp/toy/python/1202. Smallest String With Swaps.py
@@ -4,23 +4,24 @@ from typing import List
 
 class Solution:
     def smallestStringWithSwaps(self, s: str, pairs: List[List[int]]) -> str:
-        parent = list(range(len(s)))
-        def find(r):
-            if parent[r] != r:
-                parent[r] = find(parent[r])
-            return r
+        p = list(range(len(s)))
 
-        for l, r in pairs:
-            pl = find(l)
-            pr = find(r)
-            if pl != pr:
-                parent[pl] = pr
+        def find(x):
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+
+        for i, j in pairs:
+            px, py = find(i), find(j)
+            if px != py:
+                p[px] = py
 
         mem = collections.defaultdict(list)
-        for counter, value in enumerate(parent):
-            mem[find(value)].append(s[counter])
-        for sets in mem:
-            mem[sets].sort(reverse = True)
+        for i, v in enumerate(p):
+            mem[find(v)].append(s[i])
+        for k in mem:
+            mem[k].sort(reverse=True)
+
         res = []
         for i in range(len(s)):
             res.append(mem[find(i)].pop())
