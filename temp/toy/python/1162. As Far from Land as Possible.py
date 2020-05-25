f8dcf64779170b3c660e864ff7ca3d2b2a9a7192
lichenAtDell <wulicheneason@gmail.com>
2020-05-25 05:59:27 -0400

    debug for maxDistance

--- temp/toy/python/1162. As Far from Land as Possible.py
+++ temp/toy/python/1162. As Far from Land as Possible.py
@@ -6,14 +6,14 @@ class Solution:
         for i in range(r):
             for j in range(c):
                 if grid[i][j]:
-                    data.append(i,j,0)
+                    data.append((i,j,0))
         d = [(1,0),(-1,0),(0,1),(0,-1)]
         while data:
             i, j , res = data.pop(0)
             for xd, yd in d:
                 x, y = i + xd, j + yd
-                if 0 <= x < r and 0 <= y < c and not grid[i][j]:
-                    grid[i][j] = 1
-                    data.append(i,j, res + 1)
+                if 0 <= x < r and 0 <= y < c and grid[x][y] == 0:
+                    grid[x][y] = 1
+                    data.append((x,y, res + 1))
         return res if res !=0 else -1
 
