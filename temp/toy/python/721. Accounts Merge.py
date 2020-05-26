7d451376b69a40d0252905aace4410bf07b9e76a
lichenAtDell <wulicheneason@gmail.com>
2020-05-26 15:43:16 -0400

    what's wrong

--- temp/toy/python/721. Accounts Merge.py
+++ temp/toy/python/721. Accounts Merge.py
@@ -15,27 +15,28 @@ class Solution:
                 if email not in em_to_id:
                     em_to_id[email] = i
                     i += 1
-                dsu.union(em_to_id[acc[1]],em_to_id[email])
+                dsu.union(em_to_id[acc[1]], em_to_id[email])
+
         ans = collections.defaultdict(list)
         for email in em_to_name:
             ans[dsu.find(em_to_id[email])].append(email)
+
         return [[em_to_name[v[0]]] + sorted(v) for v in ans.values()]
 
+    class DSU:
+        def __init__(self):
+            self.par = range(10001)
 
-class DSU:
-    def __init__(self):
-        self.par = range(10001)
+        def find(self, x):
+            if x != self.par[x]:
+                self.par[x] = self.find(self.par[x])
+            return self.par[x]
 
-    def find(self, x):
-        if  x != self.par[x]:
-            self.par[x] = self.find(self.par[x])
-        return self.par[x]
+        def union(self, x, y):
+            self.par[self.find(x)] = self.find(y)
 
-    def union(self, x, y):
-        self.par[self.find(x)] = self.find(y)
-
-    def same(self, x,y):
-        return self.find(x) == self.find(y)
+        def same(self, x, y):
+            return self.find(x) == self.find(y)
 
 if __name__ == '__main__':
     accounts = [["John", "johnsmith@mail.com", "john00@mail.com"],
