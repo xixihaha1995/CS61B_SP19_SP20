b73589461b189f4dbf6faef45d67032853174ebf
lichenAtDell <wulicheneason@gmail.com>
2020-05-26 17:56:04 -0400

    why do you use visited[i] = 1, how to find your start index

--- temp/toy/python/785. Is Graph Bipartite.py
+++ temp/toy/python/785. Is Graph Bipartite.py
@@ -3,19 +3,9 @@ from typing import List
 
 class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
-        colored = [0] * len(graph)
+        visited = [0] * len(graph)
         for i in range(len(graph)):
-            if not self.coloring(graph, i, 0,colored): return False
-        return True
-    def coloring(self, graph, node, color,colored):
-        if node in colored:
-            return colored[node] == 1
-        colored[node] = 1
-        for next in graph[node]:
-            if not self.coloring(graph, next, 1, colored): return False
-            colored[next] = -1
-        return True
-
+            visited[i] =
 
 if __name__ == '__main__':
     print(Solution().isBipartite(
