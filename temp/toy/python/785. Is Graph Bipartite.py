96c114559992ae994b26b7043127617b3aa0b03e
lichenAtDell <wulicheneason@gmail.com>
2020-05-26 19:35:50 -0400

    what's wrong

--- temp/toy/python/785. Is Graph Bipartite.py
+++ temp/toy/python/785. Is Graph Bipartite.py
@@ -6,15 +6,15 @@ class Solution:
     def isBipartite(self, graph: List[List[int]]) -> bool:
         visited = [0] * len(graph)
         for i in range(len(graph)):
-            if  visited[i] == 0:
+            if graph[i] and visited[i] == 0:
                 visited[i] = 1
                 q = collections.deque()
                 q.append(i)
                 while q:
                     v = q.popleft()
                     for next in graph[v]:
-                        if visited[next] != 0:
-                            if visited[next] == visited[v]:
+                        if visited[v] != 0:
+                            if visited[v] == visited[next]:
                                 return False
                         else:
                             visited[next] = 3 - visited[v]
