8cc2ddd303b72c65e79e39023418eaf494074411
lichenAtDell <wulicheneason@gmail.com>
2020-05-27 09:43:33 -0400

    commit for where to color

---
+++ temp/toy/python/886. Possible Bipartition.py
@@ -0,0 +1,23 @@
+class Solution:
+    def possibleBipartition(self, N: int, dislikes: List[List[int]]) -> bool:
+        graph = collections.defaultdict(list)
+        for dislike in dislikes:
+            graph[dislike[0]-1].append(dislike[1]-1)
+            graph[dislike[1]-1].append(dislike[0]-1)
+        color = [0] * N
+        for i in range(N):
+            if color[i] != 0 : continue
+
+            bfs = collections.deque()
+            bfs.append(i)
+            while bfs:
+                cur = bfs.popleft()
+                color[cur] = 1
+                for next in graph[cur]:
+                    if color[next] != 0:
+                        if color[next] == color[cur]: return False
+                    else:
+                        color[next] = -color[cur]
+                        bfs.append(next)
+        return True
+
