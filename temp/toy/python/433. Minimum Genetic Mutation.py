810bcf16af9349d71b50c4f372a8a5d20a9e123b
lichenAtDell <wulicheneason@gmail.com>
2020-05-27 11:34:01 -0400

    debug for bfs gene mutations

---
+++ temp/toy/python/433. Minimum Genetic Mutation.py
@@ -0,0 +1,14 @@
+class Solution:
+    def minMutation(self, start: str, end: str, bank: List[str]) -> int:
+        banset = set(bank)
+        bfs = collections.defaultdict(list)
+        bfs.append(start,0)
+        while bfs:
+            gene, step = bfs.popleft()
+            for i in range(len(gene)):
+                if gene == end: return i
+                for x in 'ACGT':
+                    newgene = gene[:i] + x + gene[i+1:]
+                    if newgene in banset and newgene != end:
+                        bfs.append(newgene,i+1)
+                        banset.remove(newgene)
