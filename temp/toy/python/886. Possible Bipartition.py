a9ba54d6d822d70d7a9f43783d96f02e0bf75180
lichenAtDell <wulicheneason@gmail.com>
2020-05-27 09:43:50 -0400

    yes ok

--- temp/toy/python/886. Possible Bipartition.py
+++ temp/toy/python/886. Possible Bipartition.py
@@ -10,9 +10,10 @@ class Solution:
 
             bfs = collections.deque()
             bfs.append(i)
+            color[i] = 1
             while bfs:
                 cur = bfs.popleft()
-                color[cur] = 1
+
                 for next in graph[cur]:
                     if color[next] != 0:
                         if color[next] == color[cur]: return False
