701b32b430674f972a6e45c108c798f0340ff2cb
lichenAtDell <wulicheneason@gmail.com>
2020-05-27 11:46:45 -0400

    bebug

--- temp/toy/python/433. Minimum Genetic Mutation.py
+++ temp/toy/python/433. Minimum Genetic Mutation.py
@@ -1,4 +1,5 @@
 import collections
+from typing import List
 
 
 class Solution:
@@ -7,12 +8,19 @@ class Solution:
         bfs = collections.deque()
         bfs.append((start,0))
         while bfs:
+            print(bfs)
             gene, step = bfs.popleft()
             if gene == end: return step
-            for i in range(gene):
+            for i in range(len(gene)):
                 for x in 'ACGT':
                     newgene = gene[:i] + x + gene[i+1:]
                     if newgene in banset and newgene != end:
-                        bfs.append(newgene,step + 1)
+                        bfs.append((newgene,step + 1))
                         banset.remove(newgene)
         return -1
+
+if __name__ == '__main__':
+    start="AACCGGTT"
+    end="AAACGGTA"
+    bank= ["AACCGGTA", "AACCGCTA", "AAACGGTA"]
+    print(Solution().minMutation(start,end,bank))
