326029fd8c923fc9da5987f354c2648be9a5bf94
lichenAtDell <wulicheneason@gmail.com>
2020-05-29 15:20:13 -0400

    a debugger for last commit

--- temp/toy/python/332. Reconstruct Itinerary.py
+++ temp/toy/python/332. Reconstruct Itinerary.py
@@ -3,15 +3,18 @@ from typing import List
 
 class Solution:
     def findItinerary(self, tickets: List[List[str]]) -> List[str]:
-        edge = collections.defualtdict(list)
+        edge = collections.defaultdict(list)
         for fro, to in tickets:
             edge[fro].append(to)
-        to.sort(reverse = True)
+        for frm, to in edge.items():
+            to.sort(reverse=True)
         res = []
-        self.dfs(edge, source, res)
-        return res
+        self.dfs(edge, "JFK", res)
+        return res[::-1]
+
     def dfs(self, edge, source, res):
-        v = edge[source].pop()
+
         while edge[source]:
+            v = edge[source].pop()
             self.dfs(edge, v, res)
         res.append(source)
