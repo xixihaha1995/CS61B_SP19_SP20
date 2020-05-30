fc765133bb1124c8ada8a15c05716d513b29e146
lichenAtDell <wulicheneason@gmail.com>
2020-05-30 15:34:24 -0400

    word ladder debug

--- temp/toy/python/127. Word Ladder.py
+++ temp/toy/python/127. Word Ladder.py
@@ -1,20 +1,20 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
+        wordset = set(wordList)
         q = []
         q.append((beginWord, 1))
         while q:
             curr = q.pop()
             currword = curr[0]
             currlen = curr[1]
+            if currword == endWord: return currlen
             for i in range(len(currword)):
-                if currword == endWord: return currlen
-                part1 = currword[ : i]
-                part2 = currword[i+1 : ]
                 for j in 'abcdefghijklmnopqrstuvwxyz':
-                    if j != currword[i]:
-                        nextword = part1 + j + part2
-                        if nextword in wordList:
-                            wordList.remove(nextword)
-                            q.append((nextword, currlen+1))
+                    part1 = currword[: i]
+                    part2 = currword[i + 1:]
 
-
+                    nextword = part1 + j + part2
+                    if nextword in wordList and nextword != currword:
+                        wordset.remove(nextword)
+                        q.append((nextword, currlen+1))
+        return 0
