8d0a7de8a429b8b40f2a8a202d77a33acd89cc79
lichenAtDell <wulicheneason@gmail.com>
2020-05-31 12:33:25 -0400

    yes ok

--- temp/toy/python/842. Split Array into Fibonacci Sequence.py
+++ temp/toy/python/842. Split Array into Fibonacci Sequence.py
@@ -3,12 +3,15 @@ class Solution:
         res = []
         self.dfs(S, res, [])
         return res
+
     def dfs(self, str, res, path):
-        if len(path) >= 3 and path[-1] != path[2] + path[1]: return False
-        if not str and len(path) >= 3: res.extend(path)
+        if len(path) >= 3 and path[-1] != path[-2] + path[-3]: return False
+        if not str and len(path) >= 3:
+            res.extend(path)
+            return True
 
         for i in range(len(str)):
-            curr  = str[:i+1]
-            if (curr[0] == '0' and len(curr) != 1) or int(curr) >= 2**31: continue
-            if self.dfs(str[i+1:], path + [int(curr)], res): return True
+            curr = str[:i + 1]
+            if (curr[0] == '0' and len(curr) != 1) or int(curr) >= 2 ** 31: continue
+            if self.dfs(str[i + 1:], res, path + [int(curr)]): return True
         return False
