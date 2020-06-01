97e92f61e7d38a285654ebcc5fcbfdfbd3d87155
lichenAtDell <wulicheneason@gmail.com>
2020-06-01 09:08:44 -0400

    3sum debugged

--- temp/toy/python/15. 3Sum.py
+++ temp/toy/python/15. 3Sum.py
@@ -9,12 +9,12 @@ class Solution:
             while i < j:
                 _sum = nums[t] + nums[i] + nums[j]
                 if _sum == 0:
-                    res.append(t,i,j)
+                    res.append([nums[t], nums[i], nums[j]])
                     i += 1
                     j -= 1
-                    while i < j and nums[i] == nums[i+1]:
+                    while i < j and nums[i] == nums[i-1]:
                         i += 1
-                    while i < j and nums[j] == nums[j-1]:
+                    while i < j and nums[j] == nums[j+1]:
                         j -= 1
                 elif _sum < 0:
                     i += 1
