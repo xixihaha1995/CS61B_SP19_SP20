06fbfab0d4416cdca240d546d7f9d59e21dc4c4a
lichenAtDell <wulicheneason@gmail.com>
2020-06-02 19:11:44 -0400

    so many test cases

--- temp/toy/python/221. Maximal Square.py
+++ temp/toy/python/221. Maximal Square.py
@@ -3,22 +3,17 @@ from typing import List
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
-        if not matrix:
-            return 0
-        m = len(matrix)
-        n = len(matrix[0])
-        dp = [[0]* n for _ in range(m)]
+        m, n = len(matrix), len(matrix[0])
         for i in range(m):
-            dp[i][0] = int(matrix[i][0])
-        for j in range(n):
-            dp[0][j] = int(matrix[0][j])
-        for i in range(1,m):
-            for j in range(1,n):
-                if int(matrix[i][j]) == 1:
-                    dp[i][j] = min(dp[i-1][j], dp[i][j-1],dp[i-1][j-1])+1
-        return max(map(max, dp))**2
+            for j in range(n):
+                print(matrix)
+                if i != 0 and j != 0:
+                    if matrix[i][j] != 0:
+                        matrix[i][j] = str(min(int(matrix[i-1][j]), int(matrix[i][j-1])) + 1)
+
+        return max(matrix)
 
 if __name__ == '__main__':
     print(Solution().maximalSquare(
-
+        [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
     ))
