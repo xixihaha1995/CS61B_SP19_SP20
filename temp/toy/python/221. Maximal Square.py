d8a59dc8ff850bd5af559fbab1436b585714152b
lichenAtDell <wulicheneason@gmail.com>
2020-06-02 19:22:20 -0400

    yes, it's running

--- temp/toy/python/221. Maximal Square.py
+++ temp/toy/python/221. Maximal Square.py
@@ -3,17 +3,27 @@ from typing import List
 
 class Solution:
     def maximalSquare(self, matrix: List[List[str]]) -> int:
+        if not matrix: return 0
+        maxnum = 0
+
         m, n = len(matrix), len(matrix[0])
         for i in range(m):
+            temp = int(matrix[i][0])
+            maxnum = max(maxnum, temp)
+        for j in range(n):
+            temp = int(matrix[0][j])
+            maxnum = max(maxnum, temp)
+        for i in range(m):
             for j in range(n):
-                print(matrix)
                 if i != 0 and j != 0:
-                    if matrix[i][j] != 0:
-                        matrix[i][j] = str(min(int(matrix[i-1][j]), int(matrix[i][j-1])) + 1)
+                    if matrix[i][j] != '0':
+                        temp = min(int(matrix[i - 1][j]), int(matrix[i][j - 1]), int(matrix[i - 1][j - 1])) + 1
+                        maxnum = max(maxnum, temp)
+                        matrix[i][j] = str(temp)
 
-        return max(matrix)
+        return maxnum ** 2
 
 if __name__ == '__main__':
     print(Solution().maximalSquare(
-        [["1", "0", "1", "0", "0"], ["1", "0", "1", "1", "1"], ["1", "1", "1", "1", "1"], ["1", "0", "0", "1", "0"]]
+        [["1", "0", "1", "0"], ["1", "0", "1", "1"], ["1", "0", "1", "1"], ["1", "1", "1", "1"]]
     ))
