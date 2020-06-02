2a5ac97d78ea86fae7b1df6e0a333045ae89318d
lichenAtDell <wulicheneason@gmail.com>
2020-06-02 13:45:21 -0400

    2D indexes

--- temp/toy/python/63. Unique Paths II.py
+++ temp/toy/python/63. Unique Paths II.py
@@ -3,27 +3,19 @@ from typing import List
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        m = len(obstacleGrid)
-        n = len(obstacleGrid[0])
-
-        memo = [ [0]* n for _ in range(m)]
-
-        memo[0][0] = 1
-        for i in range (m):
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0]* n for _ in range(m) ]
+        if obstacleGrid[0][0] == 0:
+            dp[0][0]=1
+        for i in range(m):
             for j in range(n):
                 if obstacleGrid[i][j] == 1:
-                    memo[i][j] = 0
+                    dp[i][j] = 0
                 else:
-                    if i != 0 :
-                        memo[i][j]  += memo[i-1][j]
-                    if j != 0:
-                        memo[i][j] += memo[i][j-1]
-        return memo[m-1][n-1]
+                    if i != 0: dp[i][j] += dp[i-1][j]
+                    if j != 0: dp[i][j] += dp[i][j-1]
+        return dp[-1][-1]
 if __name__ == '__main__':
     print(Solution().uniquePathsWithObstacles(
-        [
-            [0, 1, 0],
-            [0, 0, 0],
-            [0, 0, 0]
-        ]
+        [[1,0]]
     ))
