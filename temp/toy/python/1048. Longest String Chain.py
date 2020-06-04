396ee94de676626f88b0537f5627180e0ff1906f
lichenAtDell <wulicheneason@gmail.com>
2020-06-04 13:01:25 -0400

    commit for longest string chain

---
+++ temp/toy/python/1048. Longest String Chain.py
@@ -0,0 +1,26 @@
+from typing import List
+
+
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        if not words: return 0
+        n = len(words)
+        dp = [1] * n
+        longest = 1
+        for i in range(1,n):
+            for j in range(i):
+                if len(words[i]) == len(words[j]) + 1:
+                    for char in words[j]:
+                        if char not in words[i]:
+                            continue
+                    dp[i] = dp[j] + 1
+                    longest = max(longest, dp[i])
+        return longest
+
+
+if __name__ == '__main__':
+    print(
+        Solution().longestStrChain(
+            ["a", "b", "ba", "bca", "bda", "bdca"]
+        )
+    )
--- temp/toy/python/300. Longest Increasing Subsequence.py
+++ temp/toy/python/300. Longest Increasing Subsequence.py
@@ -1,22 +1,25 @@
 import collections
+import copy
 from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         if not nums: return 0
-        dp = collections.defaultdict(list)
+        dp = collections.defaultdict(set)
         longest = 1
         for i in range(len(nums)):
-            dp[nums[i]].append(nums[i])
+            dp[nums[i]].add(nums[i])
             for j in range(i):
                 if nums[i] > nums[j] and len(dp[nums[i]]) <= len(dp[nums[j]]):
-                    dp[nums[i]] = dp[nums[j]][:]
-                    dp[nums[i]].append(nums[i])
-                    longest = max(longest, len(nums))
+                    dp[nums[i]] = copy.deepcopy(dp[nums[j]])
+                    dp[nums[i]].add(nums[i])
+                    print(dp[nums[i]])
+                    longest = max(longest, len(dp[nums[i]]))
         return longest
 
 
 if __name__ == '__main__':
     # print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
-    print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
+    # print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
+    print(Solution().lengthOfLIS([4, 10, 4, 3, 8, 9]))
