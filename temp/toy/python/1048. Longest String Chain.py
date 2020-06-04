72ed12bd9e6dd07a5b6659786dc4c4ee5f2c4391
lichenAtDell <wulicheneason@gmail.com>
2020-06-04 13:51:57 -0400

    cmp in python2 key in python3

--- temp/toy/python/1048. Longest String Chain.py
+++ temp/toy/python/1048. Longest String Chain.py
@@ -1,27 +1,47 @@
+import functools
 from typing import List
 
 
 class Solution:
     def longestStrChain(self, words: List[str]) -> int:
         if not words: return 0
-        words.sort()
+
+        def mycmp(x, y):
+            return len(x) - len(y)
+
+        words.sort(key=functools.cmp_to_key(mycmp))
         n = len(words)
         dp = [1] * n
         longest = 1
-        for i in range(1,n):
+        for i in range(0, n):
             for j in range(i):
-                if len(words[i]) == len(words[j]) + 1:
-                    for char in words[j]:
-                        if char not in words[i]:
-                            continue
+                if self.diffone(words[i], words[j]):
                     dp[i] = dp[j] + 1
+                    # print(dp[i])
                     longest = max(longest, dp[i])
         return longest
+
+    def diffone(self, stra, strb):
+        if len(stra) != len(strb) + 1: return False
+        i, j = 0, 0
+        count = 0
+        while i < len(stra) and j < len(strb):
+            if stra[i] == strb[j]:
+                i += 1
+                j += 1
+            else:
+                count += 1
+                i += 1
+        return count <= 1
 
 
 if __name__ == '__main__':
     print(
         Solution().longestStrChain(
-            ["a", "b", "ba", "bca", "bda", "bdca"]
+            # ["sgtnz", "sgtz", "sgz", "ikrcyoglz", "ajelpkpx", "ajelpkpxm", "srqgtnz", "srqgotnz", "srgtnz",
+            #  "ijkrcyoglz"]
+            # ["a", "b", "ba", "bca", "bda", "bdca"]
+            ["ksqvsyq", "ks", "kss", "czvh", "zczpzvdhx", "zczpzvh", "zczpzvhx", "zcpzvh", "zczvh", "gr", "grukmj",
+             "ksqvsq", "gruj", "kssq", "ksqsq", "grukkmj", "grukj", "zczpzfvdhx", "gru"]
         )
     )
