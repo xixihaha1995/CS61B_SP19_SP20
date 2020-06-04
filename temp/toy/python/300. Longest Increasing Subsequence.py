72ade343059a4c33120621aaa7b45a28a5173127
lichenAtDell <wulicheneason@gmail.com>
2020-06-03 20:23:58 -0400

    wrong answer example

--- temp/toy/python/300. Longest Increasing Subsequence.py
+++ temp/toy/python/300. Longest Increasing Subsequence.py
@@ -1,17 +1,22 @@
+import collections
 from typing import List
 
 
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
-        dp = [1] * (len(nums))
+        dp = collections.defaultdict(list)
+        longest = []
         for i in range(len(nums)):
+            dp[nums[i]].append(nums[i])
             for j in range(i):
-                if nums[i] > nums[j]:
-                    dp[i] = max(dp[i], dp[j]+1)
-        print(dp)
-        return max(dp)
+                if nums[i] > nums[j] and len(dp[nums[i]]) <= len(dp[nums[j]]):
+                    dp[nums[i]] = dp[nums[j]][:]
+                    dp[nums[i]].append(nums[i])
+                    if len(dp[nums[i]]) > len(longest):
+                        longest = dp[nums[i]][:]
+        return len(longest)
 
 
 if __name__ == '__main__':
     # print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 7, 101, 18]))
-    print(Solution().lengthOfLIS([10, 9, 2, 5, 3, 4]))
+    print(Solution().lengthOfLIS([10,9,2,5,3,7,101,18]))
