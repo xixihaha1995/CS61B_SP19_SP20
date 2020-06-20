2e9ec1c3df624d7a9deff107d41f84708f5de265
lichenAtDell <wulicheneason@gmail.com>
2020-06-19 22:10:14 -0400

    un backup for compare of two items dp

---
+++ temp/toy/python/1139. Largest 1-Bordered Square.py
@@ -0,0 +1,44 @@
+class Solution(object):
+    def largest1BorderedSquare(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        if not grid or grid[0]: return 0
+        maxLen = 0
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    flag1 = True
+                    curlen = maxLen
+                while i + curlen < m and j + curlen < n:
+                    flag2 = True
+                    for a in range(i, i+curlen+1):
+                        if grid[a][j] != 1:
+                            flag1 = False
+                            break
+                    if not flag1: break
+                    for b in range(j, j + curlen + 1):
+                        if grid[i][b] != 1:
+                            flag1 = False
+                            break
+                    if not  flag1: break
+
+                    for a in range(i, i+curlen+1):
+                        if grid[a][j+curlen] != 1:
+                            curlen += 1
+                            flag2 = False
+                            break
+                    if not flag2: continue
+                    for b in range(j, j + curlen + 1):
+                        if grid[i+curlen][b] != 1:
+                            curlen += 1
+                            flag2 = False
+                            break
+                    if not  flag2: continue
+
+                    curlen += 1
+                    maxLen = curlen
+        return maxLen*maxLen
+
