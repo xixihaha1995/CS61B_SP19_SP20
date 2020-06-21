992b6f5fa9d77e615059304a4bc8d156d325afc3
lichenAtDell <wulicheneason@gmail.com>
2020-06-21 15:52:19 -0400

    partition with number of subarray upper boundary

--- temp/toy/python/416. Partition Equal Subset Sum.py
+++ temp/toy/python/416. Partition Equal Subset Sum.py
@@ -4,13 +4,13 @@ from typing import List
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         sumall = sum(nums)
-        if not sumall % 2: return False
+        if  sumall % 2: return False
         dp = [0] * (sumall + 1)
         dp[0] = 1
         for num in nums:
-            for i in range(sumall,0,-1):
+            for i in range(sumall,-1,-1):
                 if dp[i]: dp[i+num] = 1
-            if dp[sumall/2]: return True
+            if dp[int(sumall/2)]: return True
         return False
 
 print(Solution().canPartition(
