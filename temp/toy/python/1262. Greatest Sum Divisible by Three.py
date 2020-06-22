6c6fe6c87469682a88ea284f5b3aa3c35a93030a
lichenAtDell <wulicheneason@gmail.com>
2020-06-21 21:39:05 -0400

    debugged

--- temp/toy/python/1262. Greatest Sum Divisible by Three.py
+++ temp/toy/python/1262. Greatest Sum Divisible by Three.py
@@ -1,2 +1,15 @@
+from typing import List
+
+
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
+        dp = [0] * 3
+        for num in nums:
+            tmp = dp[:]
+            for s in tmp:
+                dp[(s + num) % 3] = max(dp[(s + num) % 3], s + num)
+        return dp[0]
+
+print(Solution().maxSumDivThree(
+    [3,6,5,1,8]
+))
