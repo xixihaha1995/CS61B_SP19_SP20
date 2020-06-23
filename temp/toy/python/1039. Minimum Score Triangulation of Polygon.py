c3e76e9b680057bc163f69646812d15bc2386d58
lichenAtDell <wulicheneason@gmail.com>
2020-06-22 22:59:25 -0400

    sleep town

--- temp/toy/python/1039. Minimum Score Triangulation of Polygon.py
+++ temp/toy/python/1039. Minimum Score Triangulation of Polygon.py
@@ -1,2 +1,15 @@
+from functools import lru_cache
+from typing import List
+
+
 class Solution:
     def minScoreTriangulation(self, A: List[int]) -> int:
+        @lru_cache(None)
+        def dp(i,j):
+            return 0 if j -i <= 1 else min(dp(i,k) + dp(k,j) + A[i] *A[k] *A[j] for k in range(i+1,j))
+
+        return dp(0, len(A) -1)
+
+print(Solution().minScoreTriangulation(
+    [1,3,1,4,1,5]
+))
