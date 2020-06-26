63a94fa661c75987bc59eb0089de01dcad1afe13
LeoPC <wulicheneason@gmail.com>
2020-06-26 12:27:40 -0400

    debugged

--- temp/toy/python/685. Redundant Connection II.py
+++ temp/toy/python/685. Redundant Connection II.py
@@ -1,26 +1,35 @@
-class UnionFind(object):
-    def __init__(self,n):
-        self.pre = list(range(n))
-    def find(self,x):
-        if x != self.pre[x]:
-            self.pre[x] = self.pre(self.pre[x])
-        return self.pre[x]
-    def union(self,x, y):
-        root2 = self.find(y)
-        root1 = self.find(x)
+# coding:utf-8
 
+class UnionFind(object):
+
+    def __init__(self, n):
+        self.pre = range(n)
+
+    def find(self, x):
+        if x != self.pre[x]:
+            self.pre[x] = self.find(self.pre[x])
+        return self.pre[x]
+
+    def union(self, x1, x2):
+        root1 = self.find(x1)
+        root2 = self.find(x2)
         if root1 != root2:
             self.pre[root2] = root1
             return False
+
         return True
 
-class Solution:
-    def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
+class Solution(object):
+    def findRedundantDirectedConnection(self, edges):
+        """
+        :type edges: List[List[int]]
+        :rtype: List[int]
+        """
         n = len(edges)
-        uf = UnionFind(n+1)
+        uf = UnionFind(n + 1)
 
         last = []
-        parent = []
+        parent = {}
         candidates = []
         for st, ed in edges:
             if ed in parent:
@@ -35,5 +44,4 @@ class Solution:
             return last
 
         return candidates[0] if last else candidates[1]
-
 
