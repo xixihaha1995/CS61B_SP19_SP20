85f718dccea33dc0d8cef0b1ac3477072f93aa64
LeoPC <wulicheneason@gmail.com>
2020-06-27 18:41:40 -0400

    debug

--- 952. Largest Component Size by Common Factor.py
+++ 952. Largest Component Size by Common Factor.py
@@ -4,33 +4,34 @@ class Solution:
         class UnionFind:
             def __init__(self, n):
                 self.parent = list(range(n))
+
             def union(self, x, y):
                 rootx = self.find(x)
                 rooty = self.find(y)
 
                 if rootx != rooty:
                     self.parent[rootx] = rooty
+
             def find(self, x):
                 while x != self.parent[x]:
                     self.parent[x] = self.parent[self.parent[x]]
                     x = self.parent[x]
                 return x
 
-            max_val = max(A)
-            unionfind = UnionFind(max_val + 1)
-            //先把所有数的factor和i 连接
-            for num in A:
-                upbound = int(sqrt(num))
-                for i in range(2, upbound + 1):
-                    if num % i == 0:
-                        unionfind.union(num, i)
-                        unionfind.union(num, num//i)
+        max_val = max(A)
+        unionfind = UnionFind(max_val + 1)
+        # 先把所有数的factor和i 连接
+        for num in A:
+            upbound = int(sqrt(num))
+            for i in range(2, upbound + 1):
+                if num % i == 0:
+                    unionfind.union(num, i)
+                    unionfind.union(num, num // i)
 
-            cnt = [0] * (max_val + 1)
-            res = 0
-            for num in A:
-                root = unionfind.find(num)
-                cnt[root] += 1
-                res = max(res, cnt[root])
-            return res
-
+        cnt = [0] * (max_val + 1)
+        res = 0
+        for num in A:
+            root = unionfind.find(num)
+            cnt[root] += 1
+            res = max(res, cnt[root])
+        return res
