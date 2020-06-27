666ce4e411d4327838cc126b86ba6c3e7c38685b
LeoPC <wulicheneason@gmail.com>
2020-06-26 20:30:17 -0400

    when a new arrow is required, update END

--- temp/toy/python/452. Minimum Number of Arrows to Burst Balloons.py
+++ temp/toy/python/452. Minimum Number of Arrows to Burst Balloons.py
@@ -3,12 +3,12 @@ class Solution:
         n = len(points)
         if n < 2: return n
 
-        points.sort(key = lambda x:x[1])
+        points.sort(key=lambda x: x[1])
 
         end = points[0][1]
         res = 1
-        for point in points:
-            if point[0] > end:
+        for i in range(1, n):
+            if points[i][0] > end:
                 res += 1
-                end = min(end, point[1])
+                end = points[i][1]
         return res
