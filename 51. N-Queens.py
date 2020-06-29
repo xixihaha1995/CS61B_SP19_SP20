8e544da0b4e0178bfe0f7daf5fe1aeb8c2bc1252
LeoPC <wulicheneason@gmail.com>
2020-06-29 09:09:55 -0400

    backtrack from labuladong debug

--- 51. N-Queens.py
+++ 51. N-Queens.py
@@ -1,38 +1,39 @@
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
-        board = [ ['.'] * n for _ in range(n)]
+        board = [['.'] * n for _ in range(n)]
         res = []
 
         def backtrack(board, row):
-            //以row的index为选择标尺
+            # //以row的index为选择标尺
             if row == len(board):
-                tmp_list = p[]
+                tmp_list = []
                 for e_row in board:
                     tmp = ''.join(e_row)
                     tmp_list.append(tmp)
                 res.append(tmp_list)
                 return
             for col in range(len(board[0])):
-                if not isValid(board, col):
+                if not isValid(board, row, col):
                     continue
-                    board[row][col] = 'Q'
-                    backtrack(board, row + 1)
-                    board[row][col] = '.'
+                board[row][col] = 'Q'
+                backtrack(board, row + 1)
+                board[row][col] = '.'
+
         def isValid(board, row, col):
             for i in range(n):
-                if board[i][col] == 'Q'
+                if board[i][col] == 'Q':
                     return False
             r_row, r_col = row, col
-            while r_row > 0 and r_col < n -1:
+            while r_row > 0 and r_col < n - 1:
                 r_row -= 1
                 r_col += 1
-                if board[r_row][r_col] == 'Q'
+                if board[r_row][r_col] == 'Q':
                     return False
             l_row, l_col = row, col
             while l_row > 0 and l_col > 0:
                 l_row -= 1
                 l_col -= 1
-                if board[l_row][l_col] == 'Q'
+                if board[l_row][l_col] == 'Q':
                     return False
             return True
 
