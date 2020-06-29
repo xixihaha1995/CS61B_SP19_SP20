672454cd08afea0f3bfe72d330531e57e9f1c2fc
LeoPC <wulicheneason@gmail.com>
2020-06-29 12:27:41 -0400

    other languages are required

---
+++ 301. Remove Invalid Parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def removeInvalidParentheses(self, s: str) -> List[str]:
+        def isValid(s:str) -> bool:
+            cnt = 0
+            for c in s:
+                if c == '(': cnt += 1
+                elif c == ')': cnt -= 1
+                if cnt < 0: return False
+            return cnt ==0
+
+        level = {s}
+        while True:
+            valid = list(filter(isValid, level))
+            if valid : return valid
+            next_level = set()
+            for item in level:
+                for i in range(len(item)):
+                    if item[i] in "()":
+                        next_level.add(item[:i] + item[i+1:])
+            level = next_level
