81ee13890faecbe8155df567e6385349b4f2bca6
LeoPC <wulicheneason@gmail.com>
2020-07-01 07:56:34 -0400

    debug grid for min sum path

--- temp/toy/python/53. Maximum Subarray.py
+++ temp/toy/python/53. Maximum Subarray.py
@@ -3,10 +3,11 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        ans = float('-inf')
         for i in range(1, len(nums)):
-            tmp = (nums[i] + nums[i-1]) if nums[i]>0 else nums[i-1]
-            nums[i] = max(tmp, nums[i]) if nums[i] > 0 else nums[i]
-        return nums[-1]
+            nums[i] = (nums[i] + nums[i-1]) if nums[i-1]>0 else nums[i]
+            ans = max(ans, nums[i])
+        return ans
 
 if __name__ == '__main__':
     print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
