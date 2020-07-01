4fed1105879ffd329120a6b1512859ea3bb41bff
LeoPC <wulicheneason@gmail.com>
2020-07-01 07:33:09 -0400

    转移状态方程

--- temp/toy/python/53. Maximum Subarray.py
+++ temp/toy/python/53. Maximum Subarray.py
@@ -3,11 +3,10 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        res = [0] * (len(nums))
-        res[0] = nums[0]
-        for i in range(1,len(nums)):
-            res[i] = max(nums[i], res[i-1] + nums[i])
-        return max(res)
+        for i in range(1, len(nums)):
+            tmp = (nums[i] + nums[i-1]) if nums[i]>0 else nums[i-1]
+            nums[i] = max(tmp, nums[i]) if nums[i] > 0 else nums[i]
+        return nums[-1]
 
 if __name__ == '__main__':
     print(Solution().maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
