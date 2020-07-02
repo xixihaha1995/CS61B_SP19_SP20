66b850bada7f932001b7c4ed94b3d4ecb0f37854
LeoPC <wulicheneason@gmail.com>
2020-07-02 17:12:39 -0400

    IO by myself diff

--- temp/toy/python/38. Count and Say.py
+++ temp/toy/python/38. Count and Say.py
@@ -1,6 +1,7 @@
 class Solution:
-    def countAndSay(selfs, n: int ) -> str:
+    def countAndSay(self, n: int) -> str:
         return "1"
+
 
 def main():
     import sys
@@ -8,18 +9,20 @@ def main():
     def readlines():
         for line in io.TextIOWrapper(sys.stdin.buffer, encoding='utf-8'):
             yield line.strip('\n')
-        lines = readlines()
-        while True:
-            try:
-                line = next(lines)
-                n = int(line);
 
-                ret = Solution().countAndSay(n)
-                out = (ret);
-                print(out)
+    lines = readlines()
+    while True:
+        try:
+            line = next(lines)
+            n = int(line);
 
-            except StopIteration:
-                break
+            ret = Solution().countAndSay(n)
+
+            out = (ret);
+            print(out)
+        except StopIteration:
+            break
+
 
 if __name__ == '__main__':
     main()
