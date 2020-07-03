81ea9b628afbf9e2113f82a8b16a297a68b72716
LeoPC <wulicheneason@gmail.com>
2020-07-03 13:57:31 -0400

    nice hh

--- temp/toy/python/ACMD.py
+++ temp/toy/python/ACMD.py
@@ -1,9 +1,4 @@
 while True:
-    line = input().split()
-    n = int(line[0])
-    if n == 0:
-        break
-    sum = 0
-    for i in range(n):
-        sum += int(line[i+1])
-    print(sum)
+    data = list(map(int, input().split()))
+    if data[0] == 0: break
+    print(sum(data[1:]))
