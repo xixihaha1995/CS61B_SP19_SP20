c0a499ef4c31d6b91a7d12c0b5e334801a173f78
lichenAtDell <wulicheneason@gmail.com>
2020-07-08 15:38:05 -0400

    build bst(children) in list with instructions

--- temp/toy/python/PAT1099.py
+++ temp/toy/python/PAT1099.py
@@ -3,6 +3,12 @@ tree = [[]for _ in range(n)]
 for i in range(n):
     tree[i].extend(map(int, input().split()))
 
-print(tree)
+
 num = sorted(map(int, input().split()))
-print(num)
+lev, cur = [0], [0]
+while cur:
+    chd = [y for x in cur for y in tree[x] if y != -1]
+    if chd:
+        lev.extend(chd)
+    cur = chd
+print(lev)
