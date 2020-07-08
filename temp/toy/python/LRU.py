2bbcea88c694d476a50dbcf2e96b0eebbacee4bd
lichenAtDell <wulicheneason@gmail.com>
2020-07-08 12:15:17 -0400

    class 后面不加括号，instance时的LRUCache(para)，para直接传递到 def __init__

--- temp/toy/python/LRU.py
+++ temp/toy/python/LRU.py
@@ -1,13 +1,13 @@
 import collections
 
-class LRUCache():
+class LRUCache:
 
     def __init__(self, capacity):
         self.capacity = capacity
         self.cache = collections.OrderedDict()
 
     def get(self, key):
-        if self.cache.has_key(key):
+        if key in self.cache:
             value = self.cache.pop(key)
             self.cache[key] = value
         else:
@@ -15,12 +15,20 @@ class LRUCache():
         return value
 
     def set(self, key, value):
-        if self.cache.has_key(key):
+        if key in self.cache:
             value = self.cache.pop(key)
             self.cache[key] = value
         else:
-            if len(self.cahce) == self.capacity:
-                self.cache.popitem( last == False )
+            if len(self.cache) == self.capacity:
+                self.cache.popitem(last = False )
                 self.cache[key] = value
             else:
                 self.cache[key] = value
+
+
+c = LRUCache(5)
+
+for i in range(5, 10):
+    c.set(i, 10 * i)
+
+print(c.cache)
