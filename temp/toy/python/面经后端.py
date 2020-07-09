8d4df5296c42aa88b8ea234e090a72f8613dd618
lichenAtDell <wulicheneason@gmail.com>
2020-07-09 08:32:20 -0400

    debugged

--- temp/toy/python/面经后端.py
+++ temp/toy/python/面经后端.py
@@ -4,6 +4,8 @@ class Solution:
         for i in range(n):
             dp[i][1] = i
         dp[0][2] = fileSize[0]
+
+        tmpMax = 0
         ans = []
         for i in range(1,len(fileSize)):
             dp[i][2] = fileSize[i] + dp[i-1][2]
@@ -12,9 +14,14 @@ class Solution:
                 dp[i][0] += 1
                 dp[i][2] -= fileSize[dp[i][0] - 1]
 
-            if dp[i][2] == target:
+            if dp[i][2] > tmpMax and dp[i][2] <= target:
+                tmpMax = dp[i][2]
                 ans = dp[i]
-        return ans if not ans else -1
+        if not ans:
+            return -1
+        else:
+            return ans
+        # return ans if not ans else -1
 
 print(Solution().copy(
     5, [1, 2, 3, 5, 4], 7
