998b04bbc8c728a6b28d35ecdb8a1402887ae426
lichenAtDell <wulicheneason@gmail.com>
2020-07-08 21:33:13 -0400

    index out of range

---
+++ temp/toy/python/889. Construct Binary Tree from Preorder and Postorder Traversal.py
@@ -0,0 +1,12 @@
+class Solution:
+    def constructFromPrePost(self, pre: List[int], post: List[int]) -> TreeNode:
+        if not (pre and post):
+            retrun None
+
+        root = TreeNode(pre[0])
+        leftRoot = pre[1]
+        leftCount = post.index(leftRoot)
+
+        root.left = self.constructFromPrePost(pre[1, 1+1+leftCount], post[: leftCount + 1 ])
+        root.right = self.constructFromPrePost(pre[2+leftCount:], post[leftCount+1 : -1])
+        return root
