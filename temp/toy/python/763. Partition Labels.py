f68d5a0f8ac576b0d83914ed5ea22146d46491bb
LeoPC <wulicheneason@gmail.com>
2020-07-16 19:29:22 -0400

    partitions labels find the largest right index, so that interval(combined with anchor and right index) can include all the characters

--- temp/toy/python/763. Partition Labels.py
+++ temp/toy/python/763. Partition Labels.py
@@ -4,7 +4,16 @@ class Solution(object):
         :type S: str
         :rtype: List[int]
         """
-        last =
+        last = {c:i for i, c in enumerate(S)}
+        anchor = j = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i-anchor + 1)
+                anchor = i + 1
+        return ans
+
 
 if __name__ == '__main__':
     print(Solution().partitionLabels(
