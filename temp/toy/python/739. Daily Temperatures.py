6660fe46ef9272a22fdfb78882aeb8ea25496f2b
lichenAtDell <wulicheneason@gmail.com>
2020-07-17 12:42:39 -0400

    index out of range exceptions

--- temp/toy/python/739. Daily Temperatures.py
+++ temp/toy/python/739. Daily Temperatures.py
@@ -3,9 +3,8 @@ class Solution:
         stack = []
         res = [0] * len(T)
         for curDay, curTemp in enumerate(T):
-            if curTemp > stack[-1][1]:
+            while stack != None and curTemp > stack[-1][1]:
                 com = stack.pop()
                 res[com[0]] = curDay - com[1]
-            else:
-                stack.append((curDay, curTemp))
+            stack.append((curDay, curTemp))
         return res
