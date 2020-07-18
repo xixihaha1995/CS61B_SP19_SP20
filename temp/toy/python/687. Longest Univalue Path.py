0840a49deb2fa20ea3b9a6a0c25f7a796f57d1b7
LeoPC <wulicheneason@gmail.com>
2020-07-18 16:18:12 -0400

    auto update self.max, return max(pl, pr)

--- temp/toy/python/687. Longest Univalue Path.py
+++ temp/toy/python/687. Longest Univalue Path.py
@@ -1,21 +1,14 @@
 class Solution:
     def longestUnivaluePath(self, root: TreeNode) -> int:
         if not root: return 0
+        self.ans = 0
         return self.leng(root)
 
     def leng(self, root):
         if not root: return 0
-        cur = 1
-        left = 0
-        right = 0
-        if root.right:
-            if root.right.val == root.val:
-                cur = cur + self.leng(root.right)
-            else:
-                right = self.leng(root.right)
-        if root.left:
-            if root.left.val == root.val:
-                cur = cur + self.leng(root.left)
-            else:
-                left = self.leng(root.left)
-        return max(cur, left, right)
+        l = self.leng(root.left) if root.left else -1
+        r = self.leng(root.right) if root.right else -1
+        pl = l + 1 if l >=0 and root.val == root.left.val else 0
+        pr = r + 1 if r >= 0 and root.val == root.right.val else 0
+        self.ans = max(self.ans, pl + pr)
+        return max(pl, pr)
