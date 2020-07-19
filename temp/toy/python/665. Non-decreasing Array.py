78c0cb8c68d3dc8b219f95ca72067a2647911942
LeoPC <wulicheneason@gmail.com>
2020-07-19 11:31:06 -0400

    non-decreasing array debugged

--- temp/toy/python/665. Non-decreasing Array.py
+++ temp/toy/python/665. Non-decreasing Array.py
@@ -2,10 +2,12 @@ class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
         flag = 0
         for i in range(1, len(nums)):
-            if nums[i]>= nums[i-1]:
+            if nums[i] >= nums[i - 1]:
                 continue
-            if i-2 >= 0 and nums[i-2] > nums[i]:
-                nums[i] = nums[i-1]
+            flag += 1
+            if i - 2 >= 0 and nums[i - 2] > nums[i]:
+                nums[i] = nums[i - 1]
             else:
-                nums[i-1] = nums[i]
-        return flag >= 1
+                nums[i - 1] = nums[i]
+        return flag <= 1
+
