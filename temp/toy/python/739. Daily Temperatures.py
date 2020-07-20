756cafc6e4aa2037fc34285c52dfe66336c57846
lichenAtDell <wulicheneason@gmail.com>
2020-07-19 22:13:41 -0400

    one give me inside giaogiao

--- temp/toy/python/739. Daily Temperatures.py
+++ temp/toy/python/739. Daily Temperatures.py
@@ -1,10 +1,16 @@
+from typing import List
+
+
 class Solution:
     def dailyTemperatures(self, T: List[int]) -> List[int]:
         stack = []
         res = [0] * len(T)
         for curDay, curTemp in enumerate(T):
-            while stack != None and curTemp > stack[-1][1]:
+            while stack and curTemp > stack[-1][1]:
                 com = stack.pop()
-                res[com[0]] = curDay - com[1]
-            stack.append((curDay, curTemp))
+                res[com[0]] = curDay - com[0]
+            stack.append([curDay, curTemp])
         return res
+print(Solution().dailyTemperatures(
+    [73,74,75,71,69,72,76,73]
+))
