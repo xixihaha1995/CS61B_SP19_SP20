0d0f7782540f305ac84263a33b3b0531b7c21b6d
LeoPC <wulicheneason@gmail.com>
2020-07-19 22:03:35 -0400

    xor is the way to find difference, such 1 ^ 2 =3

--- temp/toy/python/645. Set Mismatch.py
+++ temp/toy/python/645. Set Mismatch.py
@@ -1,8 +1,17 @@
+from typing import List
+
+
 class Solution:
     def findErrorNums(self, nums: List[int]) -> List[int]:
         s = set(nums)
         dup = sum(nums) - sum(s)
-        for i in range(nums[0], nums[-1])
-            if i in s: continue
-            else: dis = i
+        check = 0
+        for i, num in enumerate(nums, 1):
+            temp = i ^ num
+            check ^= temp
+        dis = check ^dup
         return [dup, dis]
+
+print(Solution().findErrorNums(
+    [1,2,2,4]
+))
