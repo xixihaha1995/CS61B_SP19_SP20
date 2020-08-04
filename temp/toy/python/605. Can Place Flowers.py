bf9c9f091738fe4cca81715beae134b514a25850
lichenAtDell <wulicheneason@gmail.com>
2020-08-04 16:20:13 -0400

    left and right is equal zero

---
+++ temp/toy/python/605. Can Place Flowers.py
@@ -0,0 +1,13 @@
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+
+        count = 0
+        for pos in range(len(flowerbed)):
+            if flowerbed[pos] == 0 and (pos == 0 or flowerbed[pos-1] == 0) and (pos == len(flowerbed) - 1 or flowerbed[pos+1]==0):
+                flowerbed[pos] = 1
+                count += 1
+            pos += 1
+        return count <= n
