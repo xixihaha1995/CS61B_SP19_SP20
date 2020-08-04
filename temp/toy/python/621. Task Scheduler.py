7ba49c3b0af3c11590b07f3ccc52d66896c48420
lichenAtDell <wulicheneason@gmail.com>
2020-08-04 13:47:09 -0400

    the limit elapsed time is len(tasks)

---
+++ temp/toy/python/621. Task Scheduler.py
@@ -0,0 +1,14 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        length = len(tasks)
+        if length < 1: return length
+        task_map = dict()
+        for task in tasks:
+            task_map[task] = task_map.get(task) + 1
+        task_sort = sorted(task_map, key = lambda x: -x[1])
+        maxCount = task_sort[0][1]
+        res = (maxCount - 1) * (n+1)
+        for other in task_sort:
+            if other[1] == maxCount:
+                res += 1
+        return res if res > length else length
