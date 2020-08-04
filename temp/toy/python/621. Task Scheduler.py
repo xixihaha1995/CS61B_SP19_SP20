af6cb0568974d51216c90774c2b0a8e911849990
lichenAtDell <wulicheneason@gmail.com>
2020-08-04 13:48:53 -0400

    debugged

--- temp/toy/python/621. Task Scheduler.py
+++ temp/toy/python/621. Task Scheduler.py
@@ -4,8 +4,8 @@ class Solution:
         if length < 1: return length
         task_map = dict()
         for task in tasks:
-            task_map[task] = task_map.get(task) + 1
-        task_sort = sorted(task_map, key = lambda x: -x[1])
+            task_map[task] = task_map.get(task, 0) + 1
+        task_sort = sorted(task_map.items(), key = lambda x: -x[1])
         maxCount = task_sort[0][1]
         res = (maxCount - 1) * (n+1)
         for other in task_sort:
