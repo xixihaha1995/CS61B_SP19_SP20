ba2e84e4820cceb4017ee689eb9dab673431b4fd
lichenAtDell <wulicheneason@gmail.com>
2020-08-05 18:01:22 -0400

    debugged

--- temp/toy/python/540. Single Element in a Sorted Array.py
+++ temp/toy/python/540. Single Element in a Sorted Array.py
@@ -1,16 +1,16 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
         left, right = 0, len(nums) - 1
-        while left < right:
+        while left <= right:
             mid = (left + right)//2
             if mid % 2 != 0 and mid + 1 < len(nums):
                 if nums[mid] == nums[mid+1]:
                     right = mid - 1
                 else:left = mid + 1
-            if mid % 2 == 0 and mid + 1 < len(nums):
+            elif mid % 2 == 0 and mid + 1 < len(nums):
                 if nums[mid] == nums[mid + 1]:
-                    left = mid + 2
-                else:right = mid - 2
+                    left = mid + 1
+                else:right = mid - 1
             else:
                 return nums[mid]
         return nums[left]
