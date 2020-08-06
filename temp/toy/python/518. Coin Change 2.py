5c28348039b1a0d7f5772365abe6023b7d2ac348
lichenAtDell <wulicheneason@gmail.com>
2020-08-06 14:27:43 -0400

    convert all the 1D combination of coin to 1 point

--- temp/toy/python/518. Coin Change 2.py
+++ temp/toy/python/518. Coin Change 2.py
@@ -2,16 +2,9 @@ class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
         if amount == 0 : return 1
         if not coins: return 0
-        m,n = amount+1, len(coins)
-        dp = [[0]* n for _ in range(m)]
-        dp[0] = [1] * n
-        for i in range(1,m):
-            if i % coins[0] == 0:
-                dp[i][0] = 1
-            else: dp[i][0] = 0
-        for i in range(1,m):
-            for j in range(1,n):
-                dp[i][j] = dp[i][j-1]
-                if i - coins[j] >= 0:
-                    dp[i][j] += dp[i-coins[j]][j]
-        return dp[-1][-1]
+        dp = [0]*(amount+1)
+        for coin in coins:
+            for i in range(amount+1):
+                if i < coin: continue
+                dp[i] += dp[i-coin]
+        return dp[-1]
