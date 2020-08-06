0b15cd9d49448befa4af5c646d9049301961a716
lichenAtDell <wulicheneason@gmail.com>
2020-08-06 19:04:19 -0400

    how to map from all to findNums1

---
+++ temp/toy/python/496. Next Greater Element I.py
@@ -0,0 +1,14 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [0] * len(nums2)
+        store =dict()
+        stack = []
+        for i in range(len(nums2) - 1, -1, -1):
+            while stack and stack[-1] <= nums2[i]:
+                stack.pop()
+            res[i] = stack[-1] if stack else -1
+            map[nums2[i]] = res[i]
+            stack.append(nums2[i])
+        for i in range(len(nums1)):
+            nums1[i] =map[nums1[i]]
+        return nums1
