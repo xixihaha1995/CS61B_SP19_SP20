0b0647b6379f7074420ca837ffa22e262fcc23c3
lichenAtDell <wulicheneason@gmail.com>
2020-08-06 13:41:19 -0400

    not algo

--- temp/toy/python/524. Longest Word in Dictionary through Deleting.py
+++ temp/toy/python/524. Longest Word in Dictionary through Deleting.py
@@ -3,14 +3,14 @@ class Solution:
 
         ans, anslen = "", 0
         for dStr in d:
-            i, j =0
+            i, j = 0, 0
             while i < len(s) and j < len(dStr):
                 if s[i] == dStr[j]:
                     i += 1
                     j += 1
                 else: i += 1
                 if j == len(dStr):
-                    if j > len(dStr) or ( j== len(dStr) and dStr < ans):
+                    if j > len(ans) or ( j== len(ans) and dStr < ans):
                         ans = dStr
                         anslen = j
         return ans
