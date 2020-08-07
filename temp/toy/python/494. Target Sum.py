7d66973c6d68151e9767f6be8bc2f6455d9c101a
lichenAtDell <wulicheneason@gmail.com>
2020-08-06 20:22:55 -0400

    tql

--- temp/toy/python/494. Target Sum.py
+++ temp/toy/python/494. Target Sum.py
@@ -2,10 +2,10 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], S: int) -> int:
         allPos = sum(nums)
         if (allPos + S) % 2 != 0 or S > allPos: return 0
-        cap = (allPos + S ) /2
+        cap = int((allPos + S ) /2)
         dp = [0] * (cap + 1)
         dp[0] = 1
-        for num in nums:
-            for i in range(cap, -1, -1):
-                dp[i] += dp[i-nums]
+        for ele in nums:
+            for i in range(cap, ele-1, -1):
+                dp[i] += dp[i-ele]
         return dp[cap]
