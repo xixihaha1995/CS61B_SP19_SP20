cd66ea65f00db571fa83c6c1f68a77811a77ee60
lichenAtDell <wulicheneason@gmail.com>
2020-08-08 13:27:12 -0400

    when consecutive intervals is interrupted, update current distance

--- temp/toy/python/413. Arithmetic Slices.py
+++ temp/toy/python/413. Arithmetic Slices.py
@@ -1,11 +1,13 @@
 class Solution:
     def numberOfArithmeticSlices(self, A: List[int]) -> int:
-        dp = [0] * len(A):
+        if len(A) < 3: return 0
+        dp = [0] * len(A)
         temp = A[1] - A[0]
         for i in range(2,len(A)):
             curTemp = A[i] - A[i-1]
             if curTemp == temp:
-                dp[i] += dp[i-1]
+                dp[i] = 1+ dp[i-1]
+            else:
                 temp = curTemp
         return sum(dp)
 
