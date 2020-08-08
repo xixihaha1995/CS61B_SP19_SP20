9f7419f28880e9d6eb81b14cea77314a34071a81
lichenAtDell <wulicheneason@gmail.com>
2020-08-07 21:27:57 -0400

    right to explorer, left to exploit

---
+++ temp/toy/python/438. Find All Anagrams in a String.py
@@ -0,0 +1,35 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        need = dict()
+        window = dict()
+        for chara in p:
+            if chara in need:
+                need[chara] += 1
+            else:need[chara] = 1
+        left, right = 0, 0
+        valid = 0
+        res = []
+        while right < len(s):
+            curChar = s[right]
+            right += 1
+            # exploration
+            if curChar in need:
+                if curChar in window:
+                    window[curChar] += 1
+                else: window[curChar] = 1
+                if window[curChar] == need[curChar]:
+                    valid += 1
+            while right - left >= len(p):
+                if valid == len(p):
+                    res.append(left)
+                deleChar = s[left]
+                left += 1
+                if deleChar in need:
+                    if window[deleChar] == need[deleChar]:
+                        valid -= 1
+                    window[d] -= 1
+        return res
+
+
+
+
