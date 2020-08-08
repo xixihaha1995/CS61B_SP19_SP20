93673ff99bf9c0b02cc0244645de7a1ab9b6cd73
lichenAtDell <wulicheneason@gmail.com>
2020-08-08 12:00:26 -0400

    count start as 1

--- temp/toy/python/435. Non-overlapping Intervals.py
+++ temp/toy/python/435. Non-overlapping Intervals.py
@@ -1,8 +1,9 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        if not intervals : return 0
         intervals.sort(key = lambda a: a[1])
         end = intervals[0][1]
-        count = 0
+        count = 1
         for inter in intervals:
             if inter[0] >= end:
                 count += 1
