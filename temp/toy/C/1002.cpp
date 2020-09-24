fb7ab09cd9c3814658b193ef1dd32624b674f114
EC2 Default User <wulicheneason@gmail.com>
2020-09-24 07:58:28 +0000

    wait for map to put

--- temp/toy/C/1002.cpp
+++ temp/toy/C/1002.cpp
@@ -7,14 +7,18 @@ int main()
 {
 
     string mystr;
-    float price = 0;
-    int quantity=0;
+    int i, r, sum=0;
 
-    cout << "enter price\n";
+    cout << "enter a number\n";
     getline(cin, mystr);
-    stringstream(mystr) >> price;
-    cout << "enter quantity\n";
-    getline(cin, mystr);
-    stringstream(mystr) >> quantity;
-    cout << "total price:" << quantity*price << endl;
+    stringstream(mystr) >> i;
+    while(i>0)  
+    {
+        r = i % 10;
+        sum += r;
+        i /= 10;
+    }
+    
+    cout << "all the digits"<< sum << endl;
+
 }
