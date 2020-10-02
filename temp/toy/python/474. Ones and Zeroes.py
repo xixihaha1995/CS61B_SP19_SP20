fa5c7ba07484388aea1a8000de34157c342ff8ed
lichenFrom HP <wulicheneason@gmail.com>
2020-10-01 23:11:50 -0600

    debug in dp size + 1, transition equation

--- temp/toy/python/474. Ones and Zeroes.py
+++ temp/toy/python/474. Ones and Zeroes.py
@@ -1,13 +1,14 @@
+from typing import List
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        dp = [[0]* m for _ in range(n)]
+        dp = [[0]* (m + 1) for _ in range(n + 1)]
 
         for i in range(len(strs)):
             zeros = self.counter(strs[i], "0")
             ones = self.counter(strs[i], "1")
             for j in range(m,zeros - 1,-1):
                 for k in range(n, ones -1, -1):
-                    dp[j][k] = max(dp[j][k], dp[j-zeros][k-ones])
+                    dp[j][k] = max(dp[j][k], dp[j-zeros][k-ones] + 1)
         return dp[-1][-1]
 
     def counter(self, strs, num):
@@ -16,5 +17,8 @@ class Solution:
             if i == num: res += 1
         return res
 
+print(Solution().findMaxForm(
+    ["10","0001","111001","1","0"], 5, 3
+))
 
 
