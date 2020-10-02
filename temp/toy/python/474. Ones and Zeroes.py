add9ef0cc12d623fc5616d6ffe7d5ad567309c64
lichenFrom HP <wulicheneason@gmail.com>
2020-10-01 23:04:11 -0600

    python is local via pycharm

--- temp/toy/python/474. Ones and Zeroes.py
+++ temp/toy/python/474. Ones and Zeroes.py
@@ -1,20 +1,20 @@
 class Solution:
     def findMaxForm(self, strs: List[str], m: int, n: int) -> int:
-        dp = [[0]*(n + 1) for _ in range(m+1)]
-        for word in strs:
-            cal = self.countOneAndZero(word)
-            zeros = cal[0]
-            ones = cal[1]
-            for i in range(m,zeros-1,-1):
-                for j in range(n, ones-1, -1):
-                    dp[i][j] = max(dp[i][j], dp[i-zeros][j-ones] + 1)
+        dp = [[0]* m for _ in range(n)]
+
+        for i in range(len(strs)):
+            zeros = self.counter(strs[i], "0")
+            ones = self.counter(strs[i], "1")
+            for j in range(m,zeros - 1,-1):
+                for k in range(n, ones -1, -1):
+                    dp[j][k] = max(dp[j][k], dp[j-zeros][k-ones])
         return dp[-1][-1]
-    def countOneAndZero(self, word):
-        res = [0,0]
-        for charac in word:
-            if charac == '0':
-                res[0] += 1
-            else:
-                res[1] += 1
+
+    def counter(self, strs, num):
+        res = 0
+        for i in strs:
+            if i == num: res += 1
         return res
+
+
 
